bd4f317767c5a0e79b8b7031ab7e21d68f43ed0c
manhole <manholex@gmail.com>
2005-11-11 00:58:03 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@162 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/mock/servlet/MockServletContextImpl.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/mock/servlet/MockServletContextImpl.java
@@ -43,258 +43,263 @@ public class MockServletContextImpl implements MockServletContext, Serializable 
 
     private static final long serialVersionUID = -5626752218858278823L;
 
-	public static final int MAJOR_VERSION = 2;
+    public static final int MAJOR_VERSION = 2;
 
-	public static final int MINOR_VERSION = 4;
+    public static final int MINOR_VERSION = 4;
 
-	public static final String SERVER_INFO = "seasar/2.0";
+    public static final String SERVER_INFO = "seasar/2.0";
 
-	private String path_;
+    private String path_;
 
-	private Map mimeTypes_ = new HashMap();
+    private Map mimeTypes_ = new HashMap();
 
-	private Map initParameters_ = new HashMap();
+    private Map initParameters_ = new HashMap();
 
-	private Map attributes_ = new HashMap();
+    private Map attributes_ = new HashMap();
 
-	public MockServletContextImpl(String path) {
-		if (path == null) {
-			path = "/";
-		}
-		if (path.charAt(0) == '/') {
-			path_ = path;
-		} else {
-			path_ = "/" + path;
-		}
-	}
+    public MockServletContextImpl(String path) {
+        if (path == null) {
+            path = "/";
+        }
+        if (path.charAt(0) == '/') {
+            path_ = path;
+        } else {
+            path_ = "/" + path;
+        }
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getContext(java.lang.String)
-	 */
-	public ServletContext getContext(String path) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @see javax.servlet.ServletContext#getContext(java.lang.String)
+     */
+    public ServletContext getContext(String path) {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getMajorVersion()
-	 */
-	public int getMajorVersion() {
-		return MAJOR_VERSION;
-	}
+    /**
+     * @see javax.servlet.ServletContext#getMajorVersion()
+     */
+    public int getMajorVersion() {
+        return MAJOR_VERSION;
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getMinorVersion()
-	 */
-	public int getMinorVersion() {
-		return MINOR_VERSION;
-	}
+    /**
+     * @see javax.servlet.ServletContext#getMinorVersion()
+     */
+    public int getMinorVersion() {
+        return MINOR_VERSION;
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getMimeType(java.lang.String)
-	 */
-	public String getMimeType(String file) {
-		return (String) mimeTypes_.get(file);
-	}
+    /**
+     * @see javax.servlet.ServletContext#getMimeType(java.lang.String)
+     */
+    public String getMimeType(String file) {
+        return (String) mimeTypes_.get(file);
+    }
 
-	public void addMimeType(String file, String type) {
-		mimeTypes_.put(file, type);
-	}
+    public void addMimeType(String file, String type) {
+        mimeTypes_.put(file, type);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getResourcePaths(java.lang.String)
-	 */
-	public Set getResourcePaths(String path) {
-		File src = ResourceUtil.getResourceAsFile(".");
-		File root = src.getParentFile();
-		if (root.getName().equalsIgnoreCase("WEB-INF")) {
-			root = root.getParentFile();
-		}
-		File file = new File(root, adjustPath(path));
-		if (!file.exists()) {
-			String[] array = StringUtil.split(path, "/");
-			if (array.length > 1) {
-				file = new File(root, array[array.length - 1]);
-			}
-		}
-		if (file.isDirectory()) {
-			Set paths = new HashSet();
-			File[] files = file.listFiles();
-			if (files != null) {
-				for (int i = 0; i < files.length; ++i) {
-					paths.add("file:/" + files[i].getAbsolutePath());
-				}
-				return paths;
-			}
-		}
-		return null;
-	}
+    /**
+     * @see javax.servlet.ServletContext#getResourcePaths(java.lang.String)
+     */
+    public Set getResourcePaths(String path) {
+        File src = ResourceUtil.getResourceAsFile(".");
+        File root = src.getParentFile();
+        if (root.getName().equalsIgnoreCase("WEB-INF")) {
+            root = root.getParentFile();
+        }
+        File file = new File(root, adjustPath(path));
+        if (!file.exists()) {
+            String[] array = StringUtil.split(path, "/");
+            if (array.length > 1) {
+                path = array[array.length - 1];
+            }
+            do {
+                file = new File(root, path);
+                root = root.getParentFile();
+            } while (!file.exists() && root != null);
+        }
+        if (file.isDirectory()) {
+            Set paths = new HashSet();
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (int i = 0; i < files.length; ++i) {
+                    paths.add("file:/" + files[i].getAbsolutePath());
+                }
+                return paths;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getResource(java.lang.String)
-	 */
-	public URL getResource(String path) throws MalformedURLException {
-		path = adjustPath(path);
-		if (ResourceUtil.isExist(path)) {
-			return ResourceUtil.getResource(path);
-		}
-		return new URL(path);
-	}
+    /**
+     * @see javax.servlet.ServletContext#getResource(java.lang.String)
+     */
+    public URL getResource(String path) throws MalformedURLException {
+        path = adjustPath(path);
+        if (ResourceUtil.isExist(path)) {
+            return ResourceUtil.getResource(path);
+        }
+        return new URL(path);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getResourceAsStream(java.lang.String)
-	 */
-	public InputStream getResourceAsStream(String path) {
-		return ResourceUtil.getResourceAsStream(adjustPath(path));
-	}
-	
-	protected String adjustPath(String path) {
-		if (path != null && path.length() >= 0 && path.charAt(0) == '/') {
-			return path.substring(1);
-		}
-		return path;
-	}
+    /**
+     * @see javax.servlet.ServletContext#getResourceAsStream(java.lang.String)
+     */
+    public InputStream getResourceAsStream(String path) {
+        return ResourceUtil.getResourceAsStream(adjustPath(path));
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getRequestDispatcher(java.lang.String)
-	 */
-	public RequestDispatcher getRequestDispatcher(String path) {
-		return new MockRequestDispatcherImpl();
-	}
+    protected String adjustPath(String path) {
+        if (path != null && path.length() >= 0 && path.charAt(0) == '/') {
+            return path.substring(1);
+        }
+        return path;
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getNamedDispatcher(java.lang.String)
-	 */
-	public RequestDispatcher getNamedDispatcher(String name) {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @see javax.servlet.ServletContext#getRequestDispatcher(java.lang.String)
+     */
+    public RequestDispatcher getRequestDispatcher(String path) {
+        return new MockRequestDispatcherImpl();
+    }
 
-	/**
+    /**
+     * @see javax.servlet.ServletContext#getNamedDispatcher(java.lang.String)
+     */
+    public RequestDispatcher getNamedDispatcher(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
      * @deprecated
-	 * @see javax.servlet.ServletContext#getServlet(java.lang.String)
-	 */
-	public Servlet getServlet(String name) throws ServletException {
-		throw new UnsupportedOperationException();
-	}
+     * @see javax.servlet.ServletContext#getServlet(java.lang.String)
+     */
+    public Servlet getServlet(String name) throws ServletException {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
+    /**
      * @deprecated
-	 * @see javax.servlet.ServletContext#getServlets()
-	 */
-	public Enumeration getServlets() {
-		throw new UnsupportedOperationException();
-	}
+     * @see javax.servlet.ServletContext#getServlets()
+     */
+    public Enumeration getServlets() {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
+    /**
      * @deprecated
-	 * @see javax.servlet.ServletContext#getServletNames()
-	 */
-	public Enumeration getServletNames() {
-		throw new UnsupportedOperationException();
-	}
+     * @see javax.servlet.ServletContext#getServletNames()
+     */
+    public Enumeration getServletNames() {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#log(java.lang.String)
-	 */
-	public void log(String message) {
-		System.out.println(message);
-	}
+    /**
+     * @see javax.servlet.ServletContext#log(java.lang.String)
+     */
+    public void log(String message) {
+        System.out.println(message);
+    }
 
-	/**
+    /**
      * @deprecated
-	 * @see javax.servlet.ServletContext#log(java.lang.Exception,
-	 *      java.lang.String)
-	 */
-	public void log(Exception ex, String message) {
-		System.out.println(message);
-		ex.printStackTrace();
-	}
+     * @see javax.servlet.ServletContext#log(java.lang.Exception,
+     *      java.lang.String)
+     */
+    public void log(Exception ex, String message) {
+        System.out.println(message);
+        ex.printStackTrace();
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#log(java.lang.String,
-	 *      java.lang.Throwable)
-	 */
-	public void log(String message, Throwable t) {
-		System.out.println(message);
-		t.printStackTrace();
-	}
+    /**
+     * @see javax.servlet.ServletContext#log(java.lang.String,
+     *      java.lang.Throwable)
+     */
+    public void log(String message, Throwable t) {
+        System.out.println(message);
+        t.printStackTrace();
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getRealPath(java.lang.String)
-	 */
-	public String getRealPath(String path) {
-		return ResourceUtil.getResource(adjustPath(path)).getFile();
-	}
+    /**
+     * @see javax.servlet.ServletContext#getRealPath(java.lang.String)
+     */
+    public String getRealPath(String path) {
+        return ResourceUtil.getResource(adjustPath(path)).getFile();
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getServerInfo()
-	 */
-	public String getServerInfo() {
-		return SERVER_INFO;
-	}
+    /**
+     * @see javax.servlet.ServletContext#getServerInfo()
+     */
+    public String getServerInfo() {
+        return SERVER_INFO;
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getInitParameter(java.lang.String)
-	 */
-	public String getInitParameter(String name) {
-		return (String) initParameters_.get(name);
-	}
+    /**
+     * @see javax.servlet.ServletContext#getInitParameter(java.lang.String)
+     */
+    public String getInitParameter(String name) {
+        return (String) initParameters_.get(name);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getInitParameterNames()
-	 */
-	public Enumeration getInitParameterNames() {
-		return new EnumerationAdapter(initParameters_.keySet().iterator());
-	}
+    /**
+     * @see javax.servlet.ServletContext#getInitParameterNames()
+     */
+    public Enumeration getInitParameterNames() {
+        return new EnumerationAdapter(initParameters_.keySet().iterator());
+    }
 
-	public void setInitParameter(String name, String value) {
-		initParameters_.put(name, value);
-	}
+    public void setInitParameter(String name, String value) {
+        initParameters_.put(name, value);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getAttribute(java.lang.String)
-	 */
-	public Object getAttribute(String name) {
-		return attributes_.get(name);
-	}
+    /**
+     * @see javax.servlet.ServletContext#getAttribute(java.lang.String)
+     */
+    public Object getAttribute(String name) {
+        return attributes_.get(name);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getAttributeNames()
-	 */
-	public Enumeration getAttributeNames() {
-		return new EnumerationAdapter(attributes_.keySet().iterator());
-	}
+    /**
+     * @see javax.servlet.ServletContext#getAttributeNames()
+     */
+    public Enumeration getAttributeNames() {
+        return new EnumerationAdapter(attributes_.keySet().iterator());
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#setAttribute(java.lang.String,
-	 *      java.lang.Object)
-	 */
-	public void setAttribute(String name, Object value) {
-		attributes_.put(name, value);
-	}
+    /**
+     * @see javax.servlet.ServletContext#setAttribute(java.lang.String,
+     *      java.lang.Object)
+     */
+    public void setAttribute(String name, Object value) {
+        attributes_.put(name, value);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#removeAttribute(java.lang.String)
-	 */
-	public void removeAttribute(String name) {
-		attributes_.remove(name);
-	}
+    /**
+     * @see javax.servlet.ServletContext#removeAttribute(java.lang.String)
+     */
+    public void removeAttribute(String name) {
+        attributes_.remove(name);
+    }
 
-	/**
-	 * @see javax.servlet.ServletContext#getServletContextName()
-	 */
-	public String getServletContextName() {
-		return path_;
-	}
-	
-	public MockHttpServletRequestImpl createRequest(String path) {
-		String queryString = null;
+    /**
+     * @see javax.servlet.ServletContext#getServletContextName()
+     */
+    public String getServletContextName() {
+        return path_;
+    }
+
+    public MockHttpServletRequestImpl createRequest(String path) {
+        String queryString = null;
         int question = path.indexOf('?');
         if (question >= 0) {
-        	queryString = path.substring(question + 1);
+            queryString = path.substring(question + 1);
             path = path.substring(0, question);
         }
         MockHttpServletRequestImpl request = new MockHttpServletRequestImpl(this, path);
         request.setQueryString(queryString);
         return request;
-	}
+    }
+
 }
