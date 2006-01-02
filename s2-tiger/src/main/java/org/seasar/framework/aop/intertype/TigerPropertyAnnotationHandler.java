1b83e29e267426495aba4fe816e9fa7ac4e66e54
Koichi Kobayashi <koichik@improvement.jp>
2006-01-02 17:40:12 +0000

    added PropertyInterType
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@303 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/framework/aop/intertype/TigerPropertyAnnotationHandler.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.intertype;
+
+import java.lang.reflect.Field;
+
+import org.seasar.framework.aop.intertype.PropertyInterType.PropertyAnnotationHandler;
+import org.seasar.framework.container.annotation.tiger.Property;
+import org.seasar.framework.container.annotation.tiger.PropertyType;
+
+/**
+ * @author y-komori
+ * 
+ */
+public class TigerPropertyAnnotationHandler implements PropertyAnnotationHandler {
+
+    public boolean isPropertyAnnotatted(Field field) {
+        Property property = field.getAnnotation(Property.class);
+        return (property != null) ? true : false;
+    }
+
+    public int getPropertyType(Field field) {
+        Property property = field.getAnnotation(Property.class);
+        int propertyType = PropertyInterType.NONE;
+        if (property != null) {
+            PropertyType type = property.value();
+            if (type == PropertyType.NONE) {
+                propertyType = PropertyInterType.NONE;
+            } else if (type == PropertyType.READ) {
+                propertyType = PropertyInterType.READ;
+            } else if (type == PropertyType.WRITE) {
+                propertyType = PropertyInterType.WRITE;
+            } else if (type == PropertyType.READWRITE) {
+                propertyType = PropertyInterType.READWRITE;
+            }
+        }
+
+        return propertyType;
+    }
+}
---
+++ s2-tiger/src/test/java/org/seasar/framework/aop/intertype/TigerPropertyAnnotationHandlerTarget.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.intertype;
+
+import org.seasar.framework.container.annotation.tiger.Property;
+import org.seasar.framework.container.annotation.tiger.PropertyType;
+
+public class TigerPropertyAnnotationHandlerTarget implements PropertyInterTypeTarget {
+    @Property(PropertyType.READ)
+    int intReadField_ = 123;
+
+    @Property(PropertyType.WRITE)
+    int intWriteField_;
+
+    @Property(PropertyType.READWRITE)
+    int intReadWriteField_;
+
+    @Property(PropertyType.NONE)
+    int intNoneField_;
+
+    @Property(PropertyType.READWRITE)
+    public int publicField_;
+
+    @Property(PropertyType.READWRITE)
+    protected int protectedField_;
+
+    @Property(PropertyType.READWRITE)
+    private int privateField_;
+
+    int nonAnnotatedField_;
+
+    @Property(PropertyType.READWRITE)
+    Object objectField_;
+
+    @Property(PropertyType.READWRITE)
+    String[] stringArrayField_;
+
+    @Property(PropertyType.READWRITE)
+    long[][] longArrayField_;
+
+    public int getIntWriteField() {
+        return intWriteField_;
+    }
+}
