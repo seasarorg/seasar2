fd387f978b1280affd2cf07981054c7635255000
Koichi Kobayashi <koichik@improvement.jp>
2006-02-02 17:24:17 +0000

    fixed NotSupportedInterceptorTest to not use Japanese.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@356 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-extension/src/test/java/org/seasar/extension/tx/NotSupportedInterceptorTest.java
+++ seasar2/s2-extension/src/test/java/org/seasar/extension/tx/NotSupportedInterceptorTest.java
@@ -25,23 +25,25 @@ import org.seasar.extension.unit.S2TestCase;
 
 /**
  * 
- * @author taichi S. 
+ * @author taichi S.
  */
 public class NotSupportedInterceptorTest extends S2TestCase {
-    
+
     private static final String PATH = "NotSupportedInterceptorTest.dicon";
 
     private TxBean txBean;
-	private ExceptionBean exBean;
-	private TransactionManager tm;
-    
+
+    private ExceptionBean exBean;
+
+    private TransactionManager tm;
+
     public static void main(String[] args) {
         junit.textui.TestRunner.run(NotSupportedInterceptorTest.class);
     }
 
     public static Test suite() {
-		return new TestSuite(NotSupportedInterceptorTest.class);
-	}
+        return new TestSuite(NotSupportedInterceptorTest.class);
+    }
 
     /*
      * @see TestCase#setUp()
@@ -54,24 +56,24 @@ public class NotSupportedInterceptorTest extends S2TestCase {
     public NotSupportedInterceptorTest(String arg0) {
         super(arg0);
     }
-    
+
     public void testInvoke() throws Exception {
-        assertEquals("トランザクションが有効で無い事",false ,txBean.hasTransaction());
+        assertEquals("1", false, txBean.hasTransaction());
     }
-    
+
     public void testInvokeTx() throws Exception {
-        assertEquals("トランザクションが有効で無い事",false ,txBean.hasTransaction());
-        assertEquals("既に開始されているトランザクションが有効である事", Status.STATUS_ACTIVE, tm.getStatus());
+        assertEquals("1", false, txBean.hasTransaction());
+        assertEquals("2", Status.STATUS_ACTIVE, tm.getStatus());
     }
-    
+
     public void testInvokeExceptionTx() throws Exception {
         try {
             exBean.invoke();
-            fail("例外がAspectで握り潰されている可能性がある。");
-        } catch(Exception e) {
+            fail("1");
+        } catch (Exception e) {
             System.out.println(e);
         }
-        assertEquals("既に開始されているトランザクションが有効である事", Status.STATUS_ACTIVE, tm.getStatus());
+        assertEquals("2", Status.STATUS_ACTIVE, tm.getStatus());
     }
 
 }
