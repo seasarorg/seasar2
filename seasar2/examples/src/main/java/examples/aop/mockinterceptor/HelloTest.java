561d0f4e1f0f7e3163a4e471308be560c9d7bb4b
Koichi Kobayashi <koichik@improvement.jp>
2006-02-02 17:31:40 +0000

    fixed HelloTest to not use Japanese.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@357 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/examples/src/main/java/examples/aop/mockinterceptor/HelloTest.java
+++ seasar2/examples/src/main/java/examples/aop/mockinterceptor/HelloTest.java
@@ -19,31 +19,25 @@ import org.seasar.extension.unit.S2TestCase;
 import org.seasar.framework.aop.interceptors.MockInterceptor;
 
 public class HelloTest extends S2TestCase{
-    //テストクラスと同じパッケージにあるのでパスが省略できる
     private static String PATH = "Hello.dicon";
 	
-    //変数の自動セット
     private Hello hello ;
 	
     private MockInterceptor mi ;
 	
     public void testHello() throws Exception{
 		
-		//diconファイルでインターフェイスのモックが正しく行われているか
 		assertEquals("Hello", hello.greeting());
 		assertEquals("Hoge", hello.echo("test"));
 			
 		hello.echo("Hello");
-		//echo()メソッドが呼ばれたかどうか
 		assertEquals(true, mi.isInvoked("echo"));
 			
-		//echo()メソッドの引数の値が"Hello"かどうか
 		assertEquals("Hello", mi.getArgs("echo")[0]);
 		
     }
 	
     protected void setUp() throws Exception {
-    	//S2Containerに対するinclude()メソッド
     	include(PATH);
     }
 
