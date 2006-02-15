f2dc0003eb24de6cddb3c71dbff241c161bcf09c
manhole <manholex@gmail.com>
2006-02-15 07:53:00 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@406 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-extension/src/main/java/org/seasar/extension/dataset/states/SqlContext.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.dataset.states;
+
+/**
+ * @author higa
+ *
+ */
+public class SqlContext {
+
+    private String sql;
+    
+    private Object[] args;
+    
+    private Class[] argTypes;
+    
+    public SqlContext() {
+    }
+    
+    public SqlContext(String sql, Object[] args, Class[] argTypes) {
+        setSql(sql);
+        setArgs(args);
+        setArgTypes(argTypes);
+    }
+
+    public Object[] getArgs() {
+        return args;
+    }
+
+    public void setArgs(Object[] args) {
+        this.args = args;
+    }
+
+    public Class[] getArgTypes() {
+        return argTypes;
+    }
+
+    public void setArgTypes(Class[] argTypes) {
+        this.argTypes = argTypes;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+}
