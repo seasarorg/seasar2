f32fea33dad1be7a83eb1be76b678730c00acc61
Koichi Kobayashi <koichik@improvement.jp>
2006-02-28 18:46:55 +0000

    fixed AopProxy to correctly support return type with generics.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@458 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/test/java/org/seasar/framework/aop/proxy/AopProxyTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.proxy;
+
+import junit.framework.TestCase;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.seasar.framework.aop.Aspect;
+import org.seasar.framework.aop.Pointcut;
+import org.seasar.framework.aop.impl.AspectImpl;
+import org.seasar.framework.aop.impl.PointcutImpl;
+
+public class AopProxyTest extends TestCase {
+
+    public AopProxyTest() {
+    }
+
+    public AopProxyTest(String name) {
+        super(name);
+    }
+
+    public void testGeneric() throws Exception {
+        TestInterceptor interceptor = new TestInterceptor();
+        Pointcut pointcut = new PointcutImpl(new String[] { "get" });
+        Aspect aspect = new AspectImpl(interceptor, pointcut);
+        AopProxy aopProxy = new AopProxy(Generic.class, new Aspect[] { aspect });
+        Generic<String> generic = (Generic<String>) aopProxy.create();
+        String result = generic.get("Hoge");
+        assertEquals("1", "Hoge", result);
+        assertTrue("2", interceptor.invoked);
+    }
+
+    public static class Generic<T> {
+        public T get(T arg) {
+            return arg;
+        }
+    }
+
+    public class TestInterceptor implements MethodInterceptor {
+        private boolean invoked = false;
+
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            invoked = true;
+            return invocation.proceed();
+        }
+
+    }
+}
