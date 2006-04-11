e7fb154037734022b878897edf7fc1768eef7df2
manhole <manholex@gmail.com>
2006-04-11 11:04:21 +0000

    merged from branch -r554:555
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@557 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-extension/src/main/java/org/seasar/extension/jdbc/types/StringClobType.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jdbc.types;
+
+import java.io.StringReader;
+import java.sql.Clob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.seasar.extension.jdbc.ValueType;
+import org.seasar.framework.util.ReaderUtil;
+import org.seasar.framework.util.StringConversionUtil;
+
+/**
+ * @author manhole
+ */
+public class StringClobType implements ValueType {
+
+    public Object getValue(ResultSet resultSet, int index) throws SQLException {
+        return convertToString(resultSet.getClob(index));
+    }
+
+    public Object getValue(ResultSet resultSet, String columnName)
+            throws SQLException {
+        return convertToString(resultSet.getClob(columnName));
+    }
+
+    private String convertToString(Clob clob) throws SQLException {
+        return ReaderUtil.readText(clob.getCharacterStream());
+    }
+
+    public void bindValue(PreparedStatement ps, int index, Object value)
+            throws SQLException {
+        if (value == null) {
+            ps.setNull(index, Types.CLOB);
+        } else {
+            final String s = StringConversionUtil.toString(value);
+            ps.setCharacterStream(index, new StringReader(s), s.length());
+        }
+    }
+
+}
