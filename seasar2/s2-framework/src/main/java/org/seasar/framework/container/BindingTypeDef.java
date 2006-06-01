4b33ee60a4f122c71e4795d0b5c44ab62a5cbd9b
jundu <jundu@319488c0-e101-0410-93bc-b5e51f62721a>
2006-06-01 15:03:05 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@733 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/BindingTypeDef.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/BindingTypeDef.java
@@ -20,24 +20,78 @@ import java.lang.reflect.Field;
 import org.seasar.framework.beans.PropertyDesc;
 
 /**
- * @author higa
+ * コンポーネントをインジェクションする時の動作を表すバインディングタイプを定義します。 バインディングタイプには、 以下のようなものがあります。
  * 
+ * <dl>
+ * <dt><code>must</code></dt>
+ * <dd>自動バインディングが適用できなかった場合、 例外が発生します。</dd>
+ * <dt><code>should</code></dt>
+ * <dd>自動バインディングが適用できなかった場合、 警告を通知します。</dd>
+ * <dt><code>may</code></dt>
+ * <dd>自動バインディングが適用できなかった場合でも何もおきません。</dd>
+ * <dt><code>none</code></dt>
+ * <dd>{@link org.seasar.framework.container.AutoBindingDef 自動バインディングタイプ定義}が<code>auto</code>や<code>property</code>の場合でも自動バインディングを適用しません。</dd>
+ * </dl>
+ * 
+ * @author higa
+ * @author jundu (Javadoc)
  */
 public interface BindingTypeDef {
 
+    /**
+     * バインディングタイプ「<code>must</code>」を表す定数です。
+     */
     String MUST_NAME = "must";
 
+    /**
+     * バインディングタイプ「<code>should</code>」を表す定数です。
+     */
     String SHOULD_NAME = "should";
 
+    /**
+     * バインディングタイプ「<code>may</code>」を表す定数です。
+     */
     String MAY_NAME = "may";
 
+    /**
+     * バインディングタイプ「<code>none</code>」を表す定数です。
+     */
     String NONE_NAME = "none";
 
+    /**
+     * バインディングタイプの文字列表現を返します。
+     * 
+     * @return バインディングタイプを表す文字列
+     */
     String getName();
 
+    /**
+     * バインディングタイプに基づいて、 <code>component</code>に対してS2コンテナ上のコンポーネントをプロパティ経由でインジェクションします。
+     * 
+     * @param componentDef
+     *            コンポーネント定義
+     * @param propertyDef
+     *            プロパティに対する設定方法や設定値の定義
+     * @param propertyDesc
+     *            対象となるコンポーネントのプロパティ情報
+     * @param component
+     *            対象となるコンポーネントのインスタンス
+     */
     void bind(ComponentDef componentDef, PropertyDef propertyDef,
             PropertyDesc propertyDesc, Object component);
 
+    /**
+     * バインディングタイプに基づいて、 <code>component</code>に対してS2コンテナ上のコンポーネントをフィールドに直接インジェクションします。
+     * 
+     * @param componentDef
+     *            コンポーネント定義
+     * @param propertyDef
+     *            プロパティに対する設定方法や設定値の定義
+     * @param field
+     *            対象となるコンポーネントのフィールド情報
+     * @param component
+     *            対象となるコンポーネントのインスタンス
+     */
     void bind(ComponentDef componentDef, PropertyDef propertyDef, Field field,
             Object component);
 }
