93678fd84ef3143e19649695433e5f627ed1e2cf
nakamura-to <toshihiro.nakamura@gmail.com>
2006-06-14 20:43:20 +0000

    added S2EJB3Unit examples.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@833 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/examples/java/examples/s2ejb3unit/EmployeeDaoImpl.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.s2ejb3unit;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+
+@Stateless
+public class EmployeeDaoImpl implements EmployeeDao {
+
+    @Resource
+    DataSource ds;
+
+    public String getEmployeeName(int empno) {
+        String ename = null;
+        try {
+            Connection con = ds.getConnection();
+            try {
+                PreparedStatement ps = con
+                        .prepareStatement("SELECT ename FROM emp WHERE empno = ?");
+                try {
+                    ps.setInt(1, empno);
+                    ResultSet rs = ps.executeQuery();
+                    try {
+                        if (rs.next()) {
+                            ename = rs.getString("ename");
+                        }
+                    } finally {
+                        rs.close();
+                    }
+                } finally {
+                    ps.close();
+                }
+            } finally {
+                con.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return ename;
+    }
+}
