035ca5705c58be50364cd794a59ca54fa7ffc615
goto <goto@319488c0-e101-0410-93bc-b5e51f62721a>
2006-06-18 12:23:51 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@845 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/ExternalContext.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/ExternalContext.java
@@ -18,40 +18,148 @@ package org.seasar.framework.container;
 import java.util.Map;
 
 /**
- * @author higa
+ * <p>
+ * S2コンテナ上で、 WEBコンテナなどのコンテキストを扱うためのインターフェースです。
+ * </p>
+ * <p>
+ * {@link InstanceDef コンポーネントインスタンス定義}で、 <code>application</code>、
+ * <code>request</code>、 <code>session</code>を使う場合、
+ * {@link S2Container#init()}を行なう前に<code>ExternalContext</code>をS2コンテナに設定する必要があります。
+ * </p>
  * 
+ * @author higa
+ * @author goto(Javadoc)
  */
 public interface ExternalContext {
 
+    /**
+     * リクエストコンテキストを返します。
+     * 
+     * @return リクエストコンテキスト
+     * @see InstanceDef#REQUEST_NAME
+     * @see org.seasar.framework.container.deployer.InstanceRequestDef
+     */
     Object getRequest();
 
+    /**
+     * リクエストコンテキストを設定します。
+     * 
+     * @param request
+     *            リクエストコンテキスト
+     * @see InstanceDef#REQUEST_NAME
+     * @see org.seasar.framework.container.deployer.InstanceRequestDef
+     */
     void setRequest(Object request);
 
+    /**
+     * レスポンスコンテキストを返します。
+     * 
+     * @return レスポンスコンテキスト
+     */
     Object getResponse();
 
+    /**
+     * レスポンスコンテキストを設定します。
+     * 
+     * @param response
+     *            レスポンスコンテキスト
+     */
     void setResponse(Object response);
 
+    /**
+     * セッションコンテキストを返します。
+     * 
+     * @return セッションコンテキスト
+     * @see InstanceDef#SESSION_NAME
+     * @see org.seasar.framework.container.deployer.InstanceSessionDef
+     */
     Object getSession();
 
+    /**
+     * アプリケーションコンテキストを返します。
+     * 
+     * @return アプリケーションコンテキスト
+     * @see InstanceDef#APPLICATION_NAME
+     * @see org.seasar.framework.container.deployer.InstanceApplicationDef
+     */
     Object getApplication();
 
+    /**
+     * アプリケーションコンテキストを設定します。
+     * 
+     * @param application
+     *            アプリケーションコンテキスト
+     * @see InstanceDef#APPLICATION_NAME
+     * @see org.seasar.framework.container.deployer.InstanceApplicationDef
+     */
     void setApplication(Object application);
-    
+
+    /**
+     * アプリケーションコンテキストを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return アプリケーションコンテキスト
+     * @see InstanceDef#APPLICATION_NAME
+     * @see org.seasar.framework.container.deployer.InstanceApplicationDef
+     */
     Map getApplicationMap();
-    
+
+    /**
+     * 初期化設定値を{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return 初期化設定値
+     */
     Map getInitParameterMap();
-    
+
+    /**
+     * セッションコンテキストを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return セッションコンテキスト
+     * @see InstanceDef#SESSION_NAME
+     * @see org.seasar.framework.container.deployer.InstanceSessionDef
+     */
     Map getSessionMap();
-    
+
+    /**
+     * リクエストクッキーを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return リクエストクッキー
+     */
     Map getRequestCookieMap();
 
+    /**
+     * キーに対する値を1つ持つリクエストヘッダーを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return キーに対する値を1つ持つリクエストヘッダー
+     */
     Map getRequestHeaderMap();
 
+    /**
+     * キーに対する値を複数持つリクエストヘッダーを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return キーに対する値を複数持つリクエストヘッダー
+     */
     Map getRequestHeaderValuesMap();
-    
+
+    /**
+     * リクエストコンテキストを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return リクエストコンテキスト
+     * @see InstanceDef#REQUEST_NAME
+     * @see org.seasar.framework.container.deployer.InstanceRequestDef
+     */
     Map getRequestMap();
 
+    /**
+     * キーに対する値を1つ持つリクエストパラメータを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return キーに対する値を1つ持つリクエストパラメータ
+     */
     Map getRequestParameterMap();
-    
+
+    /**
+     * キーに対する値を複数持つリクエストパラメータを{@link java.util.Map}インターフェースで返します。
+     * 
+     * @return キーに対する値を複数持つリクエストパラメータ
+     */
     Map getRequestParameterValuesMap();
 }
