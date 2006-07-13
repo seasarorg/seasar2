cc8eb829c8b881f2afea208ab8f2531dafa0161b
belltree <belltree@319488c0-e101-0410-93bc-b5e51f62721a>
2006-07-13 14:28:51 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@986 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/AspectDefAware.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/AspectDefAware.java
@@ -16,14 +16,48 @@
 package org.seasar.framework.container;
 
 /**
- * @author higa
+ * このインターフェースは、 アスペクト定義を登録および取得することができるオブジェクトを表します。
+ * <p>
+ * アスペクト定義は複数登録することが出来ます。 アスペクト定義の取得はインデックス番号を指定して行います。
+ * <p>
  * 
+ * @author higa
+ * @author belltree (Javadoc)
+ * 
+ * @see AspectDef
  */
 public interface AspectDefAware {
 
+    /**
+     * アスペクト定義を登録(追加)します。
+     * 
+     * @param aspectDef
+     *            アスペクト定義
+     */
     public void addAspectDef(AspectDef aspectDef);
 
+    /**
+     * 登録されている{@link AspectDef アスペクト定義}の数を返します。
+     * <p>
+     * 登録されている{@link org.aopalliance.intercept.MethodInterceptor インターセプタ}の数ではなく、
+     * アスペクト定義の数を返します。 アスペクト定義のコンポーネント(インターセプタ)のクラスが
+     * {@link org.seasar.framework.aop.interceptors.InterceptorChain InterceptorChain}で、
+     * その中に複数のインターセプタが含まれる場合も、 1つのアスペクト定義としてカウントします。
+     * </p>
+     * 
+     * @return 登録されているアスペクト定義の数
+     */
     public int getAspectDefSize();
 
+    /**
+     * 指定されたインデックス番号<code>index</code>のアスペクト定義を返します。
+     * <p>
+     * インデックス番号は、 登録した順番に 0,1,2,… となります。
+     * </p>
+     * 
+     * @param index
+     *            アスペクト定義を指定するインデックス番号
+     * @return アスペクト定義
+     */
     public AspectDef getAspectDef(int index);
 }
