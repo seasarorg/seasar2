02097939b1fd523aa4ce24bc47290a29f0392239
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2006-07-15 06:17:21 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@997 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/MethodAssembler.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/MethodAssembler.java
@@ -15,11 +15,35 @@
  */
 package org.seasar.framework.container;
 
+import org.seasar.framework.exception.IllegalAccessRuntimeException;
+import org.seasar.framework.exception.InvocationTargetRuntimeException;
+
 /**
- * @author higa
+ * メソッド・インジェクションを実行してコンポーネントを組み立てます。
+ * <p>
+ * インジェクションの実行は、 {@link MethodDef メソッド定義}に基づいて行います。
+ * </p>
  * 
+ * @author higa
+ * @author azusa (JavaDoc)
  */
 public interface MethodAssembler {
 
+    /**
+     * 指定された<code>component</code>に対して、 メソッド・インジェクションを実行します。
+     * <p>
+     * メソッドの引数として指定されたコンポーネントが見つからない場合には、 IllegalMethodRuntimeExceptionがスローされます。
+     * </p>
+     * 
+     * @param component
+     *            S2コンテナ上のコンポーネントがセットされる対象
+     * @throws IllegalMethodRuntimeException
+     *             メソッドの引数として指定されたコンポーネントが見つからない場合、 およびメソッド実行時に{@link NumberFormatException}が発生した場合
+     * @throws InvocationTargetRuntimeException
+     *             メソッド実行時に検査例外が発生した場合(実行時例外とエラーが発生した場合にはそのままスローされます)
+     * @throws IllegalAccessRuntimeException
+     *             メソッド実行時に{@link IllegalAccessException}が発生した場合
+     * 
+     */
     public void assemble(Object component) throws IllegalMethodRuntimeException;
 }
