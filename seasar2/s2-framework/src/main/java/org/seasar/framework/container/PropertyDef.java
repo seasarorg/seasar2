89efb9b1236a6c80de0636a6212a252ff74caef7
maeno <maeno@319488c0-e101-0410-93bc-b5e51f62721a>
2006-07-15 18:42:00 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1013 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/PropertyDef.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/PropertyDef.java
@@ -16,18 +16,70 @@
 package org.seasar.framework.container;
 
 /**
+ * コンポーネントのプロパティまたはフィールドにインジェクションする方法を定義するインタフェースです。
+ * 
+ * <p>
+ * プロパティ定義は、diconファイルにおける<code>&lt;property&gt;</code>要素で指定されます。
+ * <code>&lt;property&gt;</code>要素にはname属性とbindingType属性が含まれています。
+ * </p>
+ * <p>
+ * <ul>
+ * <li>name属性はコンポーネントのプロパティ名またはフィールド名を指定します。</li>
+ * <li>bindingType属性はname属性にて指定されたプロパティまたはフィールドに、
+ * S2コンテナ内に格納されているコンポーネントをバインディングする際の挙動を指定します。</li>
+ * </ul>
+ * </p>
+ * <p>
+ * <code>&lt;property&gt;</code>要素の内容に指定された式またはコンポーネントが、
+ * <code>&lt;property&gt;</code>要素のname属性で指定されたプロパティまたはフィールドに設定されます。
+ * </p>
+ * <p>
+ * プロパティ定義が存在する場合のプロパティインジェクションまたはフィールドインジェクションは、
+ * diconファイルに記述されているプロパティ定義に従って行われます。
+ * プロパティ定義が存在しない場合、{@link AutoBindingDef}の定義によって自動バインディングが行われる事があります。
+ * </p>
+ * 
+ * 
  * @author higa
+ * @author Maeno(Javadoc)
  * 
  */
 public interface PropertyDef extends ArgDef {
 
+    /**
+     * インジェクション対象となるプロパティ名またはフィールド名を返します。
+     * 
+     * @return 設定対象となるプロパティ名
+     */
     public String getPropertyName();
 
+    /**
+     * バインディングタイプ定義を返します。
+     * 
+     * @return バインディングタイプ定義
+     */
     public BindingTypeDef getBindingTypeDef();
 
+    /**
+     * バインディングタイプ定義を設定します。
+     * 
+     * @param bindingTypeDef
+     *            バインディングタイプ定義
+     */
     public void setBindingTypeDef(BindingTypeDef bindingTypeDef);
 
+    /**
+     * アクセスタイプ定義を返します。
+     * 
+     * @return アクセスタイプ定義
+     */
     public AccessTypeDef getAccessTypeDef();
 
+    /**
+     * アクセスタイプ定義を設定します。
+     * 
+     * @param accessTypeDef
+     *            アクセスタイプ定義
+     */
     public void setAccessTypeDef(AccessTypeDef accessTypeDef);
 }
