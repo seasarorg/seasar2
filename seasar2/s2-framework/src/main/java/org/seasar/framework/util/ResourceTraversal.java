cb65c9004dd04beb83b5e5a5179d251033abd85c
nakamura-to <toshihiro.nakamura@gmail.com>
2006-07-17 17:07:04 +0000

    added ResourceTraversal.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1025 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/util/ResourceTraversal.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.util;
+
+import java.io.File;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * 
+ * @author taedium
+ */
+public final class ResourceTraversal {
+
+    public interface ResourceHandler {
+        void processResource(String path);
+    }
+
+    private ResourceTraversal() {
+    }
+
+    public static void forEach(final File rootDir, final ResourceHandler handler) {
+        forEach(rootDir, null, handler);
+    }
+
+    public static void forEach(final File rootDir, final String baseDirectory,
+            final ResourceHandler handler) {
+        final File baseDir = getBaseDir(rootDir, baseDirectory);
+        if (baseDir.exists()) {
+            traverseFileSystem(rootDir, baseDir, handler);
+        }
+    }
+
+    public static void forEach(final JarFile jarFile,
+            final ResourceHandler handler) {
+        final Enumeration enumeration = jarFile.entries();
+        while (enumeration.hasMoreElements()) {
+            final JarEntry entry = (JarEntry) enumeration.nextElement();
+            if (!entry.isDirectory()) {
+                final String entryName = entry.getName().replace('\\', '/');
+                handler.processResource(entryName);
+            }
+        }
+    }
+
+    private static void traverseFileSystem(final File rootDir,
+            final File baseDir, final ResourceHandler handler) {
+        final File[] files = baseDir.listFiles();
+        for (int i = 0; i < files.length; ++i) {
+            final File file = files[i];
+            if (file.isDirectory()) {
+                traverseFileSystem(rootDir, file, handler);
+            } else {
+                final int pos = FileUtil.getCanonicalPath(rootDir).length();
+                final String filePath = FileUtil.getCanonicalPath(file);
+                final String resourcePath = filePath.substring(pos + 1)
+                        .replace('\\', '/');
+                handler.processResource(resourcePath);
+            }
+        }
+    }
+
+    private static File getBaseDir(final File rootDir,
+            final String baseDirectory) {
+        File baseDir = rootDir;
+        if (baseDirectory != null) {
+            final String[] names = baseDirectory.split("/");
+            for (int i = 0; i < names.length; i++) {
+                baseDir = new File(baseDir, names[i]);
+            }
+        }
+        return baseDir;
+    }
+}
---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/util/ResourceTraversalTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.util;
+
+import java.io.File;
+import java.net.JarURLConnection;
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.seasar.framework.util.ResourceTraversal.ResourceHandler;
+
+/**
+ * @author taedium
+ * 
+ */
+public class ResourceTraversalTest extends TestCase {
+
+    private static int count = 0;
+
+    protected void setUp() throws Exception {
+        count = 0;
+    }
+
+    public void testForEachFile() throws Exception {
+        final File rootDir = ResourceUtil.getBuildDir(getClass());
+        final String path = ResourceUtil.getResourcePath(getClass());
+        final int pos = path.lastIndexOf("/");
+        final String baseDirectory = path.substring(0, pos);
+        ResourceTraversal.forEach(rootDir, baseDirectory,
+                new ResourceHandler() {
+                    public void processResource(String path) {
+                        if (count < 10) {
+                            System.out.println(path);
+                            count++;
+                        }
+                    }
+                });
+    }
+
+    public void testForEachJarFile() throws Exception {
+        final String classFilePath = TestCase.class.getName().replace('.', '/')
+                + ".class";
+        final URL classURL = ResourceUtil.getResource(classFilePath);
+        final JarURLConnection con = (JarURLConnection) classURL
+                .openConnection();
+        ResourceTraversal.forEach(con.getJarFile(), new ResourceHandler() {
+            public void processResource(String path) {
+                if (count < 10) {
+                    System.out.println(path);
+                    count++;
+                }
+            }
+        });
+    }
+
+}
