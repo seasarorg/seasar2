c135e13ca8c8733388a75e66828784860fca21c9
shot6 <shinpei.ohtani@gmail.com>
2006-08-04 06:01:02 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1103 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/aop/interceptors/ClassLoaderAwareTraceInterceptor.java
@@ -0,0 +1,74 @@
+/**
+ *
+ */
+package org.seasar.framework.aop.interceptors;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.seasar.framework.log.Logger;
+
+/**
+ * @author shot
+ */
+public class ClassLoaderAwareTraceInterceptor extends AbstractInterceptor {
+
+    private static final long serialVersionUID = 1L;
+
+    private static Logger logger = Logger.getLogger(TraceInterceptor.class);
+
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        StringBuffer buf = new StringBuffer(256);
+        Class targetClass = getTargetClass(invocation);
+        ClassLoader targetClassLoader = targetClass.getClassLoader();
+        if (targetClassLoader != null) {
+            buf.append("<");
+            buf.append(targetClassLoader.toString());
+            buf.append(">");
+        }
+        buf.append(targetClass.getName());
+        buf.append("#");
+        buf.append(invocation.getMethod().getName());
+        buf.append("(");
+        Object[] args = invocation.getArguments();
+        if (args != null && args.length > 0) {
+            for (int i = 0; i < args.length; ++i) {
+                buf.append(args[i]);
+                ClassLoader argsClassLoader = args[i].getClass()
+                        .getClassLoader();
+                if (argsClassLoader != null) {
+                    buf.append("<");
+                    buf.append(argsClassLoader.toString());
+                    buf.append(">");
+                }
+                buf.append(", ");
+            }
+            buf.setLength(buf.length() - 2);
+        }
+        buf.append(")");
+        Object ret = null;
+        Throwable cause = null;
+        logger.debug("BEGIN " + buf);
+        try {
+            ret = invocation.proceed();
+            buf.append(" : ");
+            buf.append(ret);
+            if (ret != null) {
+                ClassLoader retClassLoader = ret.getClass().getClassLoader();
+                if (retClassLoader != null) {
+                    buf.append("<");
+                    buf.append(retClassLoader.toString());
+                    buf.append(">");
+                }
+            }
+        } catch (Throwable t) {
+            buf.append(" Throwable:");
+            buf.append(t);
+            cause = t;
+        }
+        logger.debug("END " + buf);
+        if (cause == null) {
+            return ret;
+        }
+        throw cause;
+    }
+
+}
---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/aop/interceptors/ClassLoaderAwareTraceInterceptorTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.interceptors;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.seasar.framework.aop.Aspect;
+import org.seasar.framework.aop.Pointcut;
+import org.seasar.framework.aop.impl.AspectImpl;
+import org.seasar.framework.aop.impl.PointcutImpl;
+import org.seasar.framework.aop.proxy.AopProxy;
+
+/**
+ * @author shot
+ */
+public class ClassLoaderAwareTraceInterceptorTest extends TestCase {
+
+    public void testIntercept() throws Exception {
+        ClassLoaderAwareTraceInterceptor interceptor = new ClassLoaderAwareTraceInterceptor();
+        Pointcut pointcut = new PointcutImpl(new String[] { "getTime" });
+        Aspect aspect = new AspectImpl(interceptor, pointcut);
+        AopProxy aopProxy = new AopProxy(Date.class, new Aspect[] { aspect });
+        Date proxy = (Date) aopProxy.create();
+        proxy.getTime();
+    }
+
+    public void testIntercept2() throws Exception {
+        ClassLoaderAwareTraceInterceptor interceptor = new ClassLoaderAwareTraceInterceptor();
+        Pointcut pointcut = new PointcutImpl(new String[] { "hoge" });
+        Aspect aspect = new AspectImpl(interceptor, pointcut);
+        AopProxy aopProxy = new AopProxy(ThrowError.class,
+                new Aspect[] { aspect });
+        ThrowError proxy = (ThrowError) aopProxy.create();
+        try {
+            proxy.hoge();
+        } catch (Throwable ignore) {
+        }
+    }
+
+    public void testIntercept3() throws Exception {
+        ClassLoaderAwareTraceInterceptor interceptor = new ClassLoaderAwareTraceInterceptor();
+        Pointcut pointcut = new PointcutImpl(new String[] { "foo" });
+        Aspect aspect = new AspectImpl(interceptor, pointcut);
+        AopProxy aopProxy = new AopProxy(Hoge.class, new Aspect[] { aspect });
+        Hoge hoge = (Hoge) aopProxy.create();
+        hoge.foo(new Foo("a"));
+    }
+
+    public static class ThrowError {
+        public void hoge() {
+            throw new RuntimeException("hoge");
+        }
+    }
+
+    public static class Hoge {
+        public Foo foo(Foo f) {
+            return new Foo(f.getS());
+        }
+    }
+
+    public static class Foo {
+        private String s;
+
+        public Foo(String s) {
+            this.s = s;
+        }
+
+        public String getS() {
+            return s;
+        }
+    }
+}
