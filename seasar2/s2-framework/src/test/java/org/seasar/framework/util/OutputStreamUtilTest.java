8e60f4de035a0d1488ba9cf8aee2ae0698031eec
shot6 <shinpei.ohtani@gmail.com>
2006-08-07 07:17:04 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1117 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/test/java/org/seasar/framework/util/OutputStreamUtilTest.java
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/util/OutputStreamUtilTest.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.framework.container.util;
+package org.seasar.framework.util;
 
 import java.io.IOException;
 import java.io.OutputStream;
