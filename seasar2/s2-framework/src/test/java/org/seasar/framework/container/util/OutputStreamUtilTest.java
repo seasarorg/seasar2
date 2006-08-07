bff1ee57dbeff98b95bb19336d607fa180d04ead
shot6 <shinpei.ohtani@gmail.com>
2006-08-07 07:12:50 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1116 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/container/util/OutputStreamUtilTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import junit.framework.TestCase;
+
+import org.seasar.framework.exception.IORuntimeException;
+import org.seasar.framework.util.OutputStreamUtil;
+
+/**
+ * @author shot
+ */
+public class OutputStreamUtilTest extends TestCase {
+
+    public void testClose() throws Exception {
+        NotifyOutputStream out = new NotifyOutputStream();
+        OutputStreamUtil.close(out);
+        assertEquals("closed", out.getNotify());
+    }
+
+    public void testCloseNull() throws Exception {
+        OutputStreamUtil.close((OutputStream) null);
+        assertTrue(true);
+    }
+
+    public void testClose_throwIOException() throws Exception {
+        OutputStream out = new IOExceptionOccurOutputStream();
+        try {
+            OutputStreamUtil.close(out);
+            fail();
+        } catch (IORuntimeException e) {
+            assertTrue(true);
+        }
+    }
+
+    private static class NotifyOutputStream extends OutputStream {
+        private String notify_;
+
+        public void write(int arg0) throws IOException {
+        }
+
+        public void close() throws IOException {
+            super.close();
+            notify_ = "closed";
+        }
+
+        public String getNotify() {
+            return notify_;
+        }
+    }
+
+    private static class IOExceptionOccurOutputStream extends OutputStream {
+
+        public void write(int arg0) throws IOException {
+        }
+
+        public void close() throws IOException {
+            throw new IOException();
+        }
+
+    }
+}
