c9fda3576eeff4977fb7fc99b42e4475131987da
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2006-08-08 12:59:20 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1125 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/TooManyRegistrationComponentDef.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/TooManyRegistrationComponentDef.java
@@ -16,13 +16,42 @@
 package org.seasar.framework.container;
 
 /**
+ * S2コンテナ内に1つのキーで複数登録されたコンポーネントの定義を表すインタフェースです。
+ * <p>
+ * S2コンテナにコンポーネントが登録される際に、 そのキー(コンポーネントのクラス、
+ * インターフェース、あるいは名前)に対応するコンポーネントがすでに登録されていると、 コンポーネント定義が<code>TooManyRegistrationComponentDef</code>になります。
+ * </p>
+ * <p>
+ * <code>TooManyRegistrationComponentDef</code>で定義されているコンポーネントを取得しようとすると、
+ * {@link TooManyRegistrationRuntimeException}がスローされます。
+ * </p>
  * 
  * @author koichik
+ * @author azusa(JavaDoc)
+ * 
+ * @see TooManyRegistrationRuntimeException
  */
 public interface TooManyRegistrationComponentDef extends ComponentDef {
+
+    /**
+     * 同じキーで登録されたコンポーネント定義を追加します。
+     * 
+     * @param cd
+     *            同じキーで登録されたコンポーネント定義
+     */
     void addComponentDef(ComponentDef cd);
 
+    /**
+     * 複数登録されたコンポーネントの定義上のクラスの配列を返します。
+     * 
+     * @return 複数登録されたコンポーネントの定義上のクラスの配列
+     */
     Class[] getComponentClasses();
-    
+
+    /**
+     * 複数登録されたコンポーネント定義の配列を返します。
+     * 
+     * @return 複数登録されたコンポーネント定義の配列
+     */
     ComponentDef[] getComponentDefs();
 }
