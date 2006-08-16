f5ec659200b895129a9d6101e78a90ed24523b3c
nomadmonad <nomadmonad@319488c0-e101-0410-93bc-b5e51f62721a>
2006-08-16 22:54:22 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1183 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/MetaDefAware.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/MetaDefAware.java
@@ -16,18 +16,70 @@
 package org.seasar.framework.container;
 
 /**
+ * このインターフェースは、メタデータ定義を登録および取得することのできるオブジェクトを表します。 
+ * <p>
+ * メタデータ定義は複数登録することができます。 <br>
+ * 登録されているメタデータ定義は、
+ * <ul>
+ * <li>メタデータ定義定義名</li>
+ * <li>インデックス番号</li>
+ * </ul>
+ * を指定して取得することができます。
+ * </p>
  * @author higa
+ * @author Tsuyoshi Yamamoto(Javadoc)
+ * 
+ * @see MetaDef
  * 
  */
 public interface MetaDefAware {
 
+    /**
+     * メタデータ定義を登録（追加）します。
+     * 
+     * @param metaDef
+     *            メタデータ定義
+     */
     public void addMetaDef(MetaDef metaDef);
 
+    /**
+     * 登録されているメタデータ定義の数を返します。
+     * 
+     * @return 登録されているメタデータ定義の数
+     */
     public int getMetaDefSize();
 
+    /**
+     * インデックス番号<code>index</code>で指定されたメタデータ定義を返します。
+     * <p>
+     * インデックス番号は、0, 1, 2…の順に採番されます。
+     * </p>
+     * 
+     * @param index
+     *            メタデータ定義を指定するインデックス番号
+     * @return メタデータ定義
+     */
     public MetaDef getMetaDef(int index);
 
+    /**
+     * 指定したメタデータ定義名で登録されているメタデータ定義を取得します。 <br>
+     * メタデータ定義が登録されていない場合、<code>null</code>を返します。
+     * 
+     * @param name
+     *            メタデータ定義名
+     * @return メタデータ定義
+     */
     public MetaDef getMetaDef(String name);
 
+    /**
+     * 指定したメタデータ定義名で登録されているメタデータ定義を取得します。 <br>
+     * メタデータ定義が登録されていない場合、要素数0の配列を返します。
+     * @param name
+     *            メタデータ定義名
+     * 
+     * @param name
+     *            メタデータ定義名
+     * @return メタデータ定義を格納した配列
+     */
     public MetaDef[] getMetaDefs(String name);
 }
