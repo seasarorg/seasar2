d595714316891634d42ac4be41f1b19a8b87a517
Koichi Kobayashi <koichik@improvement.jp>
2006-08-19 17:17:25 +0000

    引数や戻り値を出力したくない場合のために SimpleTraceInterceptor を追加．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1186 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/aop/interceptors/SimpleTraceInterceptorTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.interceptors;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.seasar.framework.aop.Aspect;
+import org.seasar.framework.aop.Pointcut;
+import org.seasar.framework.aop.impl.AspectImpl;
+import org.seasar.framework.aop.impl.PointcutImpl;
+import org.seasar.framework.aop.proxy.AopProxy;
+
+/**
+ * @author higa
+ * 
+ */
+public class SimpleTraceInterceptorTest extends TestCase {
+
+    public void testIntercept() throws Exception {
+        SimpleTraceInterceptor interceptor = new SimpleTraceInterceptor();
+        Pointcut pointcut = new PointcutImpl(new String[] { "getTime" });
+        Aspect aspect = new AspectImpl(interceptor, pointcut);
+        AopProxy aopProxy = new AopProxy(Date.class, new Aspect[] { aspect });
+        Date proxy = (Date) aopProxy.create();
+        proxy.getTime();
+    }
+
+    public void testIntercept2() throws Exception {
+        SimpleTraceInterceptor interceptor = new SimpleTraceInterceptor();
+        Pointcut pointcut = new PointcutImpl(new String[] { "hoge" });
+        Aspect aspect = new AspectImpl(interceptor, pointcut);
+        AopProxy aopProxy = new AopProxy(ThrowError.class,
+                new Aspect[] { aspect });
+        ThrowError proxy = (ThrowError) aopProxy.create();
+        try {
+            proxy.hoge();
+        } catch (Throwable ignore) {
+        }
+    }
+
+    public static class ThrowError {
+        public void hoge() {
+            throw new RuntimeException("hoge");
+        }
+    }
+}
