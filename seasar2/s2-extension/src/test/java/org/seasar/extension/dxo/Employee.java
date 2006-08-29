24614f162a6f9357e946ffac9e4341031aafbaf8
shot6 <shinpei.ohtani@gmail.com>
2006-08-29 01:08:50 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1234 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-extension/src/test/java/org/seasar/extension/dxo/Employee.java
+++ seasar2/s2-extension/src/test/java/org/seasar/extension/dxo/Employee.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -33,7 +33,7 @@ public class Employee {
 
     private Float comm;
 
-    private Department deptno;
+    private Integer deptno;
 
     private byte[] password;
 
@@ -104,11 +104,11 @@ public class Employee {
         this.comm = comm;
     }
 
-    public Department getDeptno() {
+    public Integer getDeptno() {
         return this.deptno;
     }
 
-    public void setDeptno(Department deptno) {
+    public void setDeptno(Integer deptno) {
         this.deptno = deptno;
     }
 
--- seasar2/s2-framework/src/main/java/org/seasar/framework/beans/impl/PropertyDescImpl.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/beans/impl/PropertyDescImpl.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -38,6 +38,8 @@ import org.seasar.framework.util.TimestampConversionUtil;
  * 
  */
 public final class PropertyDescImpl implements PropertyDesc {
+
+    private static final Object[] EMPTY_ARGS = new Object[0];
 
     private String propertyName;
 
@@ -113,7 +115,7 @@ public final class PropertyDescImpl implements PropertyDesc {
     }
 
     public final Object getValue(Object target) {
-        return MethodUtil.invoke(readMethod, target, null);
+        return MethodUtil.invoke(readMethod, target, EMPTY_ARGS);
     }
 
     public final void setValue(Object target, Object value) {
