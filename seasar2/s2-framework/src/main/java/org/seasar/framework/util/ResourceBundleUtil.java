165ddf1e1e532e6063cdaf8e1a13cbd79c4d0eeb
shot6 <shinpei.ohtani@gmail.com>
2006-08-29 11:14:14 +0000

    add util for ResourceBundle.getBundle(name, locale, classLoader).
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1238 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/util/ResourceBundleUtil.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/util/ResourceBundleUtil.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -37,6 +37,18 @@ public class ResourceBundleUtil {
         }
         try {
             return ResourceBundle.getBundle(name, locale);
+        } catch (MissingResourceException ignore) {
+            return null;
+        }
+    }
+
+    public static final ResourceBundle getBundle(String name, Locale locale,
+            ClassLoader classLoader) {
+        if (locale == null) {
+            locale = Locale.getDefault();
+        }
+        try {
+            return ResourceBundle.getBundle(name, locale, classLoader);
         } catch (MissingResourceException ignore) {
             return null;
         }
--- seasar2/s2-framework/src/test/java/org/seasar/framework/util/ResourceBundleUtilTest.java
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/util/ResourceBundleUtilTest.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -30,4 +30,11 @@ public class ResourceBundleUtilTest extends TestCase {
         System.out.println(value);
         assertNotNull("1", value);
     }
+
+    public void testGetBundle() throws Exception {
+        ResourceBundle bundle = ResourceBundleUtil.getBundle("SSRMessages",
+                null, this.getClass().getClassLoader());
+        assertNotNull(bundle);
+    }
+
 }
