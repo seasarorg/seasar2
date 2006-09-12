2e70700cf36bdc63f42b8a89ec526b0963219bf0
Yasuo Higa <higayasuo@gmail.com>
2006-09-12 12:26:56 +0000

    Warmdeploy対応
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1310 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/warmdeploy/WarmdeployBehavior.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.warmdeploy;
+
+import org.seasar.framework.container.ComponentCreator;
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.container.impl.S2ContainerBehavior.DefaultProvider;
+import org.seasar.framework.convention.NamingConvention;
+
+public class WarmdeployBehavior extends DefaultProvider {
+
+    private NamingConvention namingConvention;
+
+    private ComponentCreator[] creators = new ComponentCreator[0];
+
+    public NamingConvention getNamingConvention() {
+        return namingConvention;
+    }
+
+    public void setNamingConvention(NamingConvention namingConvention) {
+        this.namingConvention = namingConvention;
+    }
+
+    public ComponentCreator[] getCreators() {
+        return creators;
+    }
+
+    public void setCreators(ComponentCreator[] creators) {
+        this.creators = creators;
+    }
+
+    protected ComponentDef getComponentDef(S2Container container, Object key) {
+        ComponentDef cd = super.getComponentDef(container, key);
+        if (cd != null) {
+            return cd;
+        }
+        if (key instanceof Class) {
+            cd = createComponentDef((Class) key);
+        } else if (key instanceof String) {
+            cd = createComponentDef((String) key);
+        } else {
+            throw new IllegalArgumentException("key");
+        }
+        if (cd != null) {
+            SingletonS2ContainerFactory.getContainer().register(cd);
+            cd.init();
+        }
+        return cd;
+    }
+
+    protected ComponentDef createComponentDef(Class componentClass) {
+        for (int i = 0; i < creators.length; ++i) {
+            ComponentCreator creator = creators[i];
+            ComponentDef cd = creator.createComponentDef(componentClass);
+            if (cd != null) {
+                return cd;
+            }
+        }
+        return null;
+    }
+
+    protected ComponentDef createComponentDef(String componentName) {
+        for (int i = 0; i < creators.length; ++i) {
+            ComponentCreator creator = creators[i];
+            ComponentDef cd = creator.createComponentDef(componentName);
+            if (cd != null) {
+                return cd;
+            }
+        }
+        return null;
+    }
+}
---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/container/warmdeploy/WarmdeployBehaviorTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.warmdeploy;
+
+import org.seasar.framework.container.ComponentCreator;
+import org.seasar.framework.container.creator.ComponentCreatorImpl;
+import org.seasar.framework.container.impl.S2ContainerBehavior;
+import org.seasar.framework.container.warmdeploy.dao.FooDao;
+import org.seasar.framework.convention.impl.NamingConventionImpl;
+import org.seasar.framework.unit.S2FrameworkTestCase;
+import org.seasar.framework.util.ClassUtil;
+
+/**
+ * @author higa
+ * 
+ */
+public class WarmdeployBehaviorTest extends S2FrameworkTestCase {
+
+    private WarmdeployBehavior behavior;
+
+    private String rootPackageName = ClassUtil.getPackageName(getClass());
+
+    protected void setUp() {
+        NamingConventionImpl convention = new NamingConventionImpl();
+        convention.addRootPackageName(rootPackageName);
+        behavior = new WarmdeployBehavior();
+        behavior.setNamingConvention(convention);
+        ComponentCreatorImpl creator = new ComponentCreatorImpl(convention);
+        creator.setNameSuffix("Dao");
+        behavior.setCreators(new ComponentCreator[] { creator });
+        S2ContainerBehavior.setProvider(behavior);
+    }
+
+    protected void tearDown() {
+        S2ContainerBehavior
+                .setProvider(new S2ContainerBehavior.DefaultProvider());
+    }
+
+    public void testCreatComponentDef_name() throws Exception {
+        assertTrue(getContainer().hasComponentDef("fooDao"));
+    }
+
+    public void testCreatComponentDef_class() throws Exception {
+        assertTrue(getContainer().hasComponentDef(FooDao.class));
+    }
+}
