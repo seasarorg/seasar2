f899673125a6ddd0b5108be14e365280b6f7d1a1
Koichi Kobayashi <koichik@improvement.jp>
2006-09-13 08:38:14 +0000

    prototype のインターセプタを間接的に利用する InterceptorAdapter を追加．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1314 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/aop/interceptors/InterceptorAdapter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.interceptors;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.seasar.framework.aop.S2MethodInvocation;
+import org.seasar.framework.aop.impl.NestedMethodInvocation;
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.util.ArrayUtil;
+
+/**
+ * @author koichik
+ */
+public class InterceptorAdapter extends AbstractInterceptor {
+
+    private static final long serialVersionUID = 1L;
+
+    protected S2Container container;
+
+    protected ComponentDef[] interceptorDefs = new ComponentDef[0];
+
+    public void setContainer(final S2Container container) {
+        this.container = container;
+    }
+
+    public void add(final String interceptorNames) {
+        interceptorDefs = (ComponentDef[]) ArrayUtil.add(interceptorDefs,
+                container.getComponentDef(interceptorNames));
+    }
+
+    public Object invoke(final MethodInvocation invocation) throws Throwable {
+        final MethodInterceptor[] interceptors = new MethodInterceptor[interceptorDefs.length];
+        for (int i = 0; i < interceptors.length; ++i) {
+            interceptors[i] = (MethodInterceptor) interceptorDefs[i]
+                    .getComponent();
+        }
+        final MethodInvocation nestInvocation = new NestedMethodInvocation(
+                (S2MethodInvocation) invocation, interceptors);
+        return nestInvocation.proceed();
+    }
+}
