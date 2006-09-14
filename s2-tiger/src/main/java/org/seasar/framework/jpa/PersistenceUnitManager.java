5e75507998f5c804b4055cc07741d75b609ad882
Koichi Kobayashi <koichik@improvement.jp>
2006-09-14 21:33:26 +0000

    PersistenceUnitManager をインタフェースと実装クラス (PersistenceUnitManagerImpl) に分離． PersistenceUnitManager の実装クラスとして static フィールドに EntityManagerFactory を保持する SingletonPersisteneUnitManagerImpl を追加．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1319 319488c0-e101-0410-93bc-b5e51f62721a

--- s2-tiger/src/main/java/org/seasar/framework/jpa/PersistenceUnitManager.java
+++ s2-tiger/src/main/java/org/seasar/framework/jpa/PersistenceUnitManager.java
@@ -1,56 +1,16 @@
 package org.seasar.framework.jpa;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.transaction.Transaction;
 
-import org.seasar.framework.container.annotation.tiger.Component;
-import org.seasar.framework.container.annotation.tiger.DestroyMethod;
+public interface PersistenceUnitManager {
 
-@Component
-public class PersistenceUnitManager {
-    private final Map<String, EntityManagerFactory> persistenceUnits = new HashMap<String, EntityManagerFactory>();
+    EntityManagerFactory getEntityManagerFactory(final String unitName);
 
-    private final Map<EntityManagerFactory, ConcurrentMap<Transaction, EntityManager>> emfContexts = new HashMap<EntityManagerFactory, ConcurrentMap<Transaction, EntityManager>>();
+    ConcurrentMap<Transaction, EntityManager> getEmfContext(
+            final EntityManagerFactory emf);
 
-    public PersistenceUnitManager() {
-    }
-
-    @DestroyMethod
-    public synchronized void close() {
-        for (final EntityManagerFactory emf : persistenceUnits.values()) {
-            emf.close();
-        }
-        persistenceUnits.clear();
-        emfContexts.clear();
-    }
-
-    public synchronized EntityManagerFactory getEntityManagerFactory(
-            final String unitName) {
-        final EntityManagerFactory emf = persistenceUnits.get(unitName);
-        if (emf != null) {
-            return emf;
-        }
-        return createEntityManagerFactory(unitName);
-    }
-
-    EntityManagerFactory createEntityManagerFactory(final String unitName) {
-        final EntityManagerFactory emf = Persistence
-                .createEntityManagerFactory(unitName);
-        persistenceUnits.put(unitName, emf);
-        emfContexts.put(emf,
-                new ConcurrentHashMap<Transaction, EntityManager>());
-        return emf;
-    }
-
-    public ConcurrentMap<Transaction, EntityManager> getEmfContext(
-            final EntityManagerFactory emf) {
-        return emfContexts.get(emf);
-    }
 }
---
+++ s2-tiger/src/main/java/org/seasar/framework/jpa/impl/SingletonPersistenceUnitManagerImpl.java
@@ -0,0 +1,66 @@
+package org.seasar.framework.jpa.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.transaction.Transaction;
+
+import org.seasar.framework.container.annotation.tiger.Component;
+import org.seasar.framework.jpa.PersistenceUnitManager;
+
+@Component
+public class SingletonPersistenceUnitManagerImpl implements
+        PersistenceUnitManager {
+
+    protected static final Map<String, EntityManagerFactory> persistenceUnits = new HashMap<String, EntityManagerFactory>();
+
+    protected static final Map<EntityManagerFactory, ConcurrentMap<Transaction, EntityManager>> emfContexts = new HashMap<EntityManagerFactory, ConcurrentMap<Transaction, EntityManager>>();
+
+    public SingletonPersistenceUnitManagerImpl() {
+    }
+
+    public void close() {
+        synchronized (getClass()) {
+            for (final EntityManagerFactory emf : persistenceUnits.values()) {
+                emf.close();
+            }
+            persistenceUnits.clear();
+            emfContexts.clear();
+        }
+    }
+
+    public EntityManagerFactory getEntityManagerFactory(final String unitName) {
+        synchronized (getClass()) {
+            final EntityManagerFactory emf = persistenceUnits.get(unitName);
+            if (emf != null) {
+                return emf;
+            }
+        }
+        return createEntityManagerFactory(unitName);
+    }
+
+    protected EntityManagerFactory createEntityManagerFactory(
+            final String unitName) {
+        final EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory(unitName);
+        synchronized (getClass()) {
+            persistenceUnits.put(unitName, emf);
+            emfContexts.put(emf,
+                    new ConcurrentHashMap<Transaction, EntityManager>());
+        }
+        return emf;
+    }
+
+    public ConcurrentMap<Transaction, EntityManager> getEmfContext(
+            final EntityManagerFactory emf) {
+        synchronized (getClass()) {
+            return emfContexts.get(emf);
+        }
+    }
+
+}
