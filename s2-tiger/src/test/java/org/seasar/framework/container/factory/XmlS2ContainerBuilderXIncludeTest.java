ebae50443698f9341baecedc17cee63bffd0f41f
Koichi Kobayashi <koichik@improvement.jp>
2006-10-06 11:26:03 +0000

    dicon ファイルで XInclude のサポートを追加 (Java5 または Xerces 2.7 以降で S2.4 の DTD のみ有効）．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1399 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/test/java/org/seasar/framework/container/factory/XmlS2ContainerBuilderXIncludeTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.factory;
+
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.unit.S2FrameworkTestCase;
+
+/**
+ * @author koichik
+ * 
+ */
+public class XmlS2ContainerBuilderXIncludeTest extends S2FrameworkTestCase {
+
+    private S2Container container;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        include(getClass().getName().replace('.', '/') + ".dicon");
+    }
+
+    public void test() {
+        S2Container child = container.getChild(0);
+        assertEquals(4, child.getComponentDefSize());
+        for (int i = 0; i < 4; ++i) {
+            ComponentDef cd = child.getComponentDef(i);
+            assertEquals("date" + (i + 1), cd.getComponentName());
+        }
+    }
+
+}
