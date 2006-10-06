f1fc46b201353c8f1d410be94790748208698919
Yasuo Higa <higayasuo@gmail.com>
2006-10-06 03:27:32 +0000

    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1392 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-extension/src/test/java/org/seasar/extension/jdbc/impl/MapArrayResultSetHandlerTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jdbc.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+
+import org.seasar.extension.jdbc.ResultSetHandler;
+import org.seasar.extension.unit.S2TestCase;
+
+public class MapArrayResultSetHandlerTest extends S2TestCase {
+
+    public void setUp() {
+        include("jdbc.dicon");
+    }
+
+    public void testHandle() throws Exception {
+        ResultSetHandler handler = new MapArrayResultSetHandler();
+        String sql = "select * from emp";
+        Connection con = getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        Map[] ret = null;
+        try {
+            ResultSet rs = ps.executeQuery();
+            try {
+                ret = (Map[]) handler.handle(rs);
+            } finally {
+                rs.close();
+            }
+        } finally {
+            ps.close();
+        }
+        assertNotNull(ret);
+        for (int i = 0; i < ret.length; ++i) {
+            System.out.println(ret[i]);
+        }
+    }
+}
