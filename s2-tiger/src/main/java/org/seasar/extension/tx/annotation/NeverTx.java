ca6106c82f42d1123ec3b5745fc6a76a6b6335e2
Koichi Kobayashi <koichik@improvement.jp>
2006-10-18 19:38:55 +0000

    @Interceptor メタアノテーションを使った S2Tx 用のアノテーションを追加．テストは．．． いいよね？
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1436 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/extension/tx/annotation/NeverTx.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.tx.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.seasar.framework.aop.annotation.Interceptor;
+
+/**
+ * @author koichik
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.TYPE, ElementType.METHOD })
+@Interceptor("j2ee.neverTx")
+public @interface NeverTx {
+
+}
