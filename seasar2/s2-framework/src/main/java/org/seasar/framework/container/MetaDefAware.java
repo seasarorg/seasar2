e2d515198741093c10f505e76d7852a7b76d8e0b
vestige <vestige@319488c0-e101-0410-93bc-b5e51f62721a>
2006-11-08 14:36:06 +0000

    modified Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1508 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/MetaDefAware.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/MetaDefAware.java
@@ -16,26 +16,19 @@
 package org.seasar.framework.container;
 
 /**
- * このインターフェースは、メタデータ定義を登録および取得することのできるオブジェクトを表します。 
+ * このインターフェースは、メタデータ定義を追加および取得することのできるオブジェクトを表します。 
  * <p>
- * メタデータ定義は複数登録することができます。 <br>
- * 登録されているメタデータ定義は、
- * <ul>
- * <li>メタデータ定義定義名</li>
- * <li>インデックス番号</li>
- * </ul>
- * を指定して取得することができます。
+ * メタデータ定義は複数追加することができます。 メタデータ定義の取得は、メタデータ定義名またはインデックス番号を指定して行います。
  * </p>
  * @author higa
- * @author Tsuyoshi Yamamoto(Javadoc)
- * 
+ * @author Tsuyoshi Yamamoto
  * @see MetaDef
  * 
  */
 public interface MetaDefAware {
 
     /**
-     * メタデータ定義を登録（追加）します。
+     * メタデータ定義を追加します。
      * 
      * @param metaDef
      *            メタデータ定義
@@ -43,16 +36,16 @@ public interface MetaDefAware {
     public void addMetaDef(MetaDef metaDef);
 
     /**
-     * 登録されているメタデータ定義の数を返します。
+     * {@link MetaDef メタデータ定義}の数を返します。
      * 
-     * @return 登録されているメタデータ定義の数
+     * @return メタデータ定義の数
      */
     public int getMetaDefSize();
 
     /**
      * インデックス番号<code>index</code>で指定されたメタデータ定義を返します。
      * <p>
-     * インデックス番号は、0, 1, 2…の順に採番されます。
+     * インデックス番号は、追加した順に0, 1, 2…となります。
      * </p>
      * 
      * @param index
@@ -74,9 +67,6 @@ public interface MetaDefAware {
     /**
      * 指定したメタデータ定義名で登録されているメタデータ定義を取得します。 <br>
      * メタデータ定義が登録されていない場合、要素数0の配列を返します。
-     * @param name
-     *            メタデータ定義名
-     * 
      * @param name
      *            メタデータ定義名
      * @return メタデータ定義を格納した配列
