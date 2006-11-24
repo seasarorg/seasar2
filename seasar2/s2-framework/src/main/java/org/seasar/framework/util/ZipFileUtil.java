f6f7dd0fa62fd3a3e4622c26a035e5e294251b50
Koichi Kobayashi <koichik@improvement.jp>
2006-11-24 11:43:22 +0000

    NamingConventionImpl で WebLogic で使われる zip: プロトコルの URL をサポートするように修正しました． svn add 忘れで r:1669 と一緒にコミットし損ねた．心より恥じる．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1670 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/util/ZipFileUtil.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.seasar.framework.exception.IORuntimeException;
+
+/**
+ * @author higa
+ * 
+ */
+public class ZipFileUtil {
+
+    private ZipFileUtil() {
+    }
+
+    public static ZipFile create(final String file) {
+        try {
+            return new ZipFile(file);
+        } catch (final IOException e) {
+            throw new IORuntimeException(e);
+        }
+    }
+
+    public static ZipFile create(final File file) {
+        try {
+            return new ZipFile(file);
+        } catch (final IOException e) {
+            throw new IORuntimeException(e);
+        }
+    }
+
+    public static InputStream getInputStream(ZipFile file, ZipEntry entry) {
+        try {
+            return file.getInputStream(entry);
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
+    }
+
+    public static ZipFile toZipFile(final URL zipUrl) {
+        return create(new File(toZipFilePath(zipUrl)));
+    }
+
+    public static String toZipFilePath(final URL zipUrl) {
+        final String urlString = zipUrl.getPath();
+        final int pos = urlString.lastIndexOf('!');
+        final String zipFilePath = urlString.substring(0, pos);
+        final File zipFile = new File(URLUtil.decode(zipFilePath, "UTF8"));
+        return FileUtil.getCanonicalPath(zipFile);
+    }
+}
---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/util/ZipFileUtilTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+import junit.framework.TestCase;
+
+/**
+ * @author koichik
+ */
+public class ZipFileUtilTest extends TestCase {
+
+    public void testToJarFilePath() throws Exception {
+        URL url = new URL(null, "zip:/Program Files/foo.zip!/",
+                new URLStreamHandler() {
+                    protected void parseURL(URL u, String spec, int start,
+                            int limit) {
+                        setURL(u, "zip", null, 0, null, null,
+                                spec.substring(4), null, null);
+                    }
+
+                    protected URLConnection openConnection(URL u)
+                            throws IOException {
+                        return null;
+                    }
+                });
+        String root = new File("/").getCanonicalPath();
+        assertEquals(root + "Program Files" + File.separator + "foo.zip",
+                ZipFileUtil.toZipFilePath(url));
+    }
+
+}
