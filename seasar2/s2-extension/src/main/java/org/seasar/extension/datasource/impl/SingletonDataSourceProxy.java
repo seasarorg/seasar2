f3087d1b0d36e645be38730e3c4d6e598b7d8d39
Koichi Kobayashi <koichik@improvement.jp>
2006-12-02 18:59:58 +0000

    メソッドが呼ばれる度に SingletonS2ContainerFactory から取得した DataSource/TransactionManager に委譲する Proxy を追加しました．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1704 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-extension/src/main/java/org/seasar/extension/datasource/impl/SingletonDataSourceProxy.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.datasource.impl;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.seasar.extension.j2ee.JndiResourceLocator;
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+
+/**
+ * @author koichik
+ */
+public class SingletonDataSourceProxy implements DataSource {
+
+    public static final String actualDataSourceName_BINDING = "bindingType=may";
+
+    protected String actualDataSourceName = "jdbc.dataSource";
+
+    public SingletonDataSourceProxy() {
+    }
+
+    public SingletonDataSourceProxy(final String actualDataSourceName) {
+        setActualDataSourceName(actualDataSourceName);
+    }
+
+    public void setActualDataSourceName(final String actualDataSourceName) {
+        this.actualDataSourceName = JndiResourceLocator
+                .resolveName(actualDataSourceName);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return getActualDataSource().getConnection();
+    }
+
+    public Connection getConnection(final String username, final String password)
+            throws SQLException {
+        return getActualDataSource().getConnection(username, password);
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return getActualDataSource().getLogWriter();
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return getActualDataSource().getLoginTimeout();
+    }
+
+    public void setLogWriter(final PrintWriter out) throws SQLException {
+        getActualDataSource().setLogWriter(out);
+    }
+
+    public void setLoginTimeout(final int seconds) throws SQLException {
+        getActualDataSource().setLoginTimeout(seconds);
+    }
+
+    protected DataSource getActualDataSource() {
+        return (DataSource) SingletonS2ContainerFactory.getContainer()
+                .getComponent(actualDataSourceName);
+    }
+
+}
---
+++ seasar2/s2-extension/src/main/java/org/seasar/extension/jta/SingletonTransactionManagerProxy.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jta;
+
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.InvalidTransactionException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+
+/**
+ * @author koichik
+ */
+public class SingletonTransactionManagerProxy implements TransactionManager {
+
+    public void begin() throws NotSupportedException, SystemException {
+        getTransactionManager().begin();
+    }
+
+    public void commit() throws HeuristicMixedException,
+            HeuristicRollbackException, IllegalStateException,
+            RollbackException, SecurityException, SystemException {
+        getTransactionManager().commit();
+    }
+
+    public int getStatus() throws SystemException {
+        return getTransactionManager().getStatus();
+    }
+
+    public Transaction getTransaction() throws SystemException {
+        return getTransactionManager().getTransaction();
+    }
+
+    public void resume(final Transaction tx) throws IllegalStateException,
+            InvalidTransactionException, SystemException {
+        getTransactionManager().resume(tx);
+    }
+
+    public void rollback() throws IllegalStateException, SecurityException,
+            SystemException {
+        getTransactionManager().rollback();
+    }
+
+    public void setRollbackOnly() throws IllegalStateException, SystemException {
+        getTransactionManager().setRollbackOnly();
+    }
+
+    public void setTransactionTimeout(final int timeout) throws SystemException {
+        getTransactionManager().setTransactionTimeout(timeout);
+    }
+
+    public Transaction suspend() throws SystemException {
+        return getTransactionManager().suspend();
+    }
+
+    protected TransactionManager getTransactionManager() {
+        return (TransactionManager) SingletonS2ContainerFactory.getContainer()
+                .getComponent(TransactionManager.class);
+    }
+
+}
