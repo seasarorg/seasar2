722dc78a345abb5ed3c0a4e7ece8672b439c93f6
nakamura-to <toshihiro.nakamura@gmail.com>
2006-12-14 17:58:28 +0000

    S2JUnit4 のアノテーションに @Inherited を注釈しました。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1764 319488c0-e101-0410-93bc-b5e51f62721a

--- s2-tiger/src/main/java/org/seasar/framework/unit/annotation/TxBehavior.java
+++ s2-tiger/src/main/java/org/seasar/framework/unit/annotation/TxBehavior.java
@@ -19,6 +19,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -28,6 +29,7 @@ import java.lang.annotation.Target;
  */
 @Target( { TYPE, METHOD })
 @Retention(RUNTIME)
+@Inherited
 public @interface TxBehavior {
     TxBehaviorType value() default TxBehaviorType.ROLLBACK;
 }
