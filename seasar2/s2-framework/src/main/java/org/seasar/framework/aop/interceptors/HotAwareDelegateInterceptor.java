5ec6482b276d5256f2a6b3cc06ca28e33333db2d
Koichi Kobayashi <koichik@improvement.jp>
2006-12-15 11:17:18 +0000

    HOT deploy に影響なく委譲するための HotAwareDelegateInterceptor を追加しました．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1766 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/aop/interceptors/HotAwareDelegateInterceptor.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.interceptors;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.exception.EmptyRuntimeException;
+import org.seasar.framework.util.MethodUtil;
+
+/**
+ * 
+ * @author koichik
+ */
+public class HotAwareDelegateInterceptor implements MethodInterceptor {
+
+    private static final long serialVersionUID = 1L;
+
+    protected S2Container container;
+
+    protected String targetName;
+
+    public HotAwareDelegateInterceptor() {
+    }
+
+    public void setContainer(S2Container container) {
+        this.container = container;
+    }
+
+    public void setTargetName(String targetName) {
+        this.targetName = targetName;
+    }
+
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        if (targetName == null) {
+            throw new EmptyRuntimeException("targetName");
+        }
+        Method method = invocation.getMethod();
+        if (!MethodUtil.isAbstract(method)) {
+            return invocation.proceed();
+        } else {
+            final Object target = container.getComponent(targetName);
+            return MethodUtil.invoke(method, target, invocation.getArguments());
+        }
+    }
+
+}
---
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/aop/interceptors/HotAwareDelegateInterceptorTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.aop.interceptors;
+
+import org.seasar.framework.unit.S2FrameworkTestCase;
+
+/**
+ * @author koichik
+ * 
+ */
+public class HotAwareDelegateInterceptorTest extends S2FrameworkTestCase {
+
+    private Hello hello;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        include("HotAwareDelegateInterceptorTest.dicon");
+    }
+
+    public void testInvoke() throws Exception {
+        assertEquals("Hello", hello.greeting());
+    }
+
+    public interface Hello {
+        public String greeting();
+    }
+
+    public static class HelloImpl implements Hello {
+        public String greeting() {
+            return "Hello";
+        }
+    }
+
+}
