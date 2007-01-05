177166dc2d3cd1352bc380080f27cd5ce51ae329
manhole <manholex@gmail.com>
2007-01-05 08:02:27 +0000

    copyrightを2004-2007に。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1810 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/external/portlet/PortletExtendedExternalContextComponentDefRegister.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/external/portlet/PortletExtendedExternalContextComponentDefRegister.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2006 the Seasar Foundation and the Others.
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-//package org.seasar.framework.container.impl.servlet;
 package org.seasar.framework.container.external.portlet;
 
 import org.seasar.framework.container.ExternalContextComponentDefRegister;
