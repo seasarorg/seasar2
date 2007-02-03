5d5e1d7074e2527d665467c8f317cbb0d8829817
jundu <jundu@319488c0-e101-0410-93bc-b5e51f62721a>
2007-02-03 03:50:43 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1894 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/PathResolver.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/PathResolver.java
@@ -16,8 +16,34 @@
 package org.seasar.framework.container.factory;
 
 /**
+ * 論理パスから物理パスを取得するためのインターフェースです。
+ * <p>
+ * <dl>
+ * <dt>論理パス</dt>
+ * <dd>コンテナの生成時やインクルード時に、 指定されたパス名です。
+ * <dt>物理パス</dt>
+ * <dd>{@link ResourceResolver}に渡されるパスです。</dd>
+ * </dl>
+ * </p>
+ * 
  * @author koichik
+ * @author jundu
  */
 public interface PathResolver {
+
+    /**
+     * 論理パスを、 物理パスに変換して返します。
+     * <p>
+     * <code>path</code>に指定するパス名がインクルードで指定されたパスの場合、 <code>context</code>はインクルード先となる親コンテナの設定ファイルの物理パスです。
+     * ルートコンテナの作成時などインクルード先の親コンテナがない場合や、 インクルード先の親コンテナが設定ファイルを持っていない場合は、
+     * <code>context</code>は<code>null</code>です。
+     * </p>
+     * 
+     * @param context
+     *            コンテキスト
+     * @param path
+     *            論理パス
+     * @return 物理パス
+     */
     String resolvePath(String context, String path);
 }
