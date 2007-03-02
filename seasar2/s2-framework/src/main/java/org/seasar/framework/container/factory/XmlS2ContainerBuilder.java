1602b4e93cb2ee520ba3c165ed6a72fd1c9ffae5
yatsu <yatsu@319488c0-e101-0410-93bc-b5e51f62721a>
2007-03-02 17:30:34 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1976 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/XmlS2ContainerBuilder.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/XmlS2ContainerBuilder.java
@@ -32,30 +32,73 @@ import org.seasar.framework.xml.SaxHandlerParser;
 import org.seasar.framework.xml.TagHandlerContext;
 
 /**
- * @author higa
+ * diconファイルから{@link org.seasar.framework.container.S2Container S2コンテナ}を構築します。
+ * <p>
+ * このクラスに対し、変更を加えることによって、S2コンテナの構築をカスタマイズすることが出来ます。 例えば、新規の{@link org.seasar.framework.xml.TagHandler}を追加した{@link org.seasar.framework.container.factory.S2ContainerTagHandlerRule}を設定することによって、
+ * 設定ファイルをカスタマイズすることが出来ます。
+ * </p>
  * 
+ * @author higa
+ * @author yatsu
  */
 public class XmlS2ContainerBuilder extends AbstractS2ContainerBuilder {
+
+    /**
+     * Seasar2.0以降でサポートされているDTDのパブリックIDです。
+     */
     public static final String PUBLIC_ID = "-//SEASAR//DTD S2Container//EN";
 
+    /**
+     * Seasar2.1以降でサポートされているDTDのパブリックIDです。
+     */
     public static final String PUBLIC_ID21 = "-//SEASAR2.1//DTD S2Container//EN";
 
+    /**
+     * Seasar2.3以降でサポートされているDTDのパブリックIDです。
+     */
     public static final String PUBLIC_ID23 = "-//SEASAR//DTD S2Container 2.3//EN";
 
+    /**
+     * Seasar2.4以降でサポートされているDTDのパブリックIDです。
+     */
     public static final String PUBLIC_ID24 = "-//SEASAR//DTD S2Container 2.4//EN";
 
+    /**
+     * diconファイルの検証に利用されるバージョン2.0のDTDのパスです。
+     */
     public static final String DTD_PATH = "org/seasar/framework/container/factory/components.dtd";
 
+    /**
+     * diconファイルの検証に利用されるバージョン2.1のDTDのパスです。
+     */
     public static final String DTD_PATH21 = "org/seasar/framework/container/factory/components21.dtd";
 
+    /**
+     * diconファイルの検証に利用されるバージョン2.3のDTDのパスです。
+     */
     public static final String DTD_PATH23 = "org/seasar/framework/container/factory/components23.dtd";
 
+    /**
+     * diconファイルの検証に利用されるバージョン2.4のDTDのパスです。
+     */
     public static final String DTD_PATH24 = "org/seasar/framework/container/factory/components24.dtd";
 
+    /**
+     * タグと<code>TagHandler</code>のマッピング情報です。
+     */
     protected S2ContainerTagHandlerRule rule = new S2ContainerTagHandlerRule();
 
+    /**
+     * 公開DTDのIDとDTDのパスのマッピング情報です。
+     */
     protected Map dtdMap = new HashMap();
 
+    /**
+     * <code>XmlS2ContainerBuilder</code>を構築します。
+     * <p>
+     * DTDマッピング情報を定義します。
+     * </p>
+     */
     public XmlS2ContainerBuilder() {
         dtdMap.put(PUBLIC_ID, DTD_PATH);
         dtdMap.put(PUBLIC_ID21, DTD_PATH21);
@@ -63,18 +106,40 @@ public class XmlS2ContainerBuilder extends AbstractS2ContainerBuilder {
         dtdMap.put(PUBLIC_ID24, DTD_PATH24);
     }
 
+    /**
+     * タグと<code>TagHandler</code>のマッピング情報を返します。
+     * 
+     * @return タグと<code>TagHandler</code>のマッピング情報
+     */
     public S2ContainerTagHandlerRule getRule() {
         return rule;
     }
 
+    /**
+     * タグと<code>TagHandler</code>のマッピング情報を設定します。
+     * 
+     * @param rule
+     *            タグと<code>TagHandler</code>のマッピング情報
+     */
     public void setRule(final S2ContainerTagHandlerRule rule) {
         this.rule = rule;
     }
 
+    /**
+     * diconの検証で使用するDTDマッピング情報を追加します。
+     * 
+     * @param publicId
+     *            DTDのパブリックID
+     * @param systemId
+     *            DTDのパス
+     */
     public void addDtd(final String publicId, final String systemId) {
         dtdMap.put(publicId, systemId);
     }
 
+    /**
+     * DTDマッピング情報を消去します。デフォルトのDTDを使用しない場合に呼び出します。
+     */
     public void clearDtd() {
         dtdMap.clear();
     }
@@ -89,6 +154,15 @@ public class XmlS2ContainerBuilder extends AbstractS2ContainerBuilder {
         return child;
     }
 
+    /**
+     * diconファイルを解析します。
+     * 
+     * @param parent
+     *            親となるS2コンテナ
+     * @param path
+     *            設定ファイルのパス
+     * @return 構築したS2コンテナ
+     */
     protected S2Container parse(final S2Container parent, final String path) {
         final SaxHandlerParser parser = createSaxHandlerParser(parent, path);
         final InputStream is = getInputStream(path);
@@ -99,6 +173,15 @@ public class XmlS2ContainerBuilder extends AbstractS2ContainerBuilder {
         }
     }
 
+    /**
+     * {@link org.seasar.framework.xml.SaxHandlerParser}を生成します。
+     * 
+     * @param parent
+     *            親となるS2コンテナ
+     * @param path
+     *            設定ファイルのパス
+     * @return 生成された<code>SaxHandlerParser</code>
+     */
     protected SaxHandlerParser createSaxHandlerParser(final S2Container parent,
             final String path) {
         final SAXParserFactory factory = SAXParserFactoryUtil.newInstance();
