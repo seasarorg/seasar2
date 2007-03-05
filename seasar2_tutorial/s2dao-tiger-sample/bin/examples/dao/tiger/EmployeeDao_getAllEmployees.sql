fd9406f5ba01e65b25719791dd5e52e414e4b19e
nakamura-to <toshihiro.nakamura@gmail.com>
2007-03-05 06:58:59 +0000

    不要なので削除。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@1989 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2_tutorial/s2dao-tiger-sample/bin/examples/dao/tiger/EmployeeDao_getAllEmployees.sql
+++
@@ -1,2 +0,0 @@
-SELECT emp.*, dept.dname dname_0, dept.loc loc_0 FROM emp, dept
-WHERE emp.deptno = dept.deptno ORDER BY emp.empno
--- seasar2_tutorial/s2dao-tiger-sample/bin/examples/dao/tiger/EmployeeDao_getEmployeeByJobDeptno.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM emp
-/*BEGIN*/WHERE
-  /*IF job != null*/job = /*job*/'CLERK'/*END*/
-  /*IF deptno != null*/AND deptno = /*deptno*/20/*END*/
-/*END*/
