d1464e04c2f9bba3e0943c84138cfa650834ee09
Koichi Kobayashi <koichik@improvement.jp>
2007-03-06 09:44:21 +0000

    Web コンテナや AP サーバの提供する DataSource と S2JTA を連携するための DataSourceXADataSource を追加しました．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2000 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/util/InitialContextUtil.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/util/InitialContextUtil.java
@@ -15,27 +15,73 @@
  */
 package org.seasar.framework.util;
 
+import java.util.Hashtable;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.seasar.framework.exception.NamingRuntimeException;
 
 /**
- * @author higa
+ * {@link javax.naming.InitialContext 初期コンテキスト}を扱うためのユーティリティ・クラスです。
  * 
+ * @author higa
  */
 public final class InitialContextUtil {
 
     private InitialContextUtil() {
     }
 
-    public static final Object lookup(InitialContext ctx, String jndiName)
-            throws NamingRuntimeException {
-
+    /**
+     * 初期コンテキストを作成して返します。
+     * 
+     * @return 初期コンテキスト
+     * @throws org.seasar.framework.exception.NamingRuntimeException
+     *             初期コンテキストを作成できなかった場合にスローされます
+     */
+    public static InitialContext create() {
         try {
-            return ctx.lookup(jndiName);
-        } catch (NamingException ex) {
+            return new InitialContext();
+        } catch (final NamingException ex) {
             throw new NamingRuntimeException(ex);
         }
     }
+
+    /**
+     * 指定した環境を使用して初期コンテキストを作成して返します。
+     * 
+     * @param env
+     *            初期コンテキストの作成に使用される環境。<code>mull</code>は空の環境を示す
+     * @return 初期コンテキスト
+     * @throws NamingRuntimeException
+     *             初期コンテキストを作成できなかった場合にスローされます
+     */
+    public static InitialContext create(final Hashtable env) {
+        try {
+            return new InitialContext(env);
+        } catch (final NamingException ex) {
+            throw new NamingRuntimeException(ex);
+        }
+    }
+
+    /**
+     * 指定した初期コンテキストから指定されたオブジェクトを取得して返します。
+     * 
+     * @param ctx
+     *            初期コンテキスト
+     * @param jndiName
+     *            検索するオブジェクトの名前
+     * @return <code>jndiName</code>にバインドされているオブジェクト
+     * @throws NamingRuntimeException
+     *             初期コンテキストを作成できなかった場合にスローされます
+     */
+    public static Object lookup(final InitialContext ctx, final String jndiName)
+            throws NamingRuntimeException {
+        try {
+            return ctx.lookup(jndiName);
+        } catch (final NamingException ex) {
+            throw new NamingRuntimeException(ex);
+        }
+    }
+
 }
