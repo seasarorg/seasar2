10fc67fb19ff3bc23a56222318c4a68e49beefd6
Hiroyuki Ohnaka <azusa@fieldnotes.jp>
2007-03-08 12:31:30 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2059 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/S2ContainerBuilder.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/S2ContainerBuilder.java
@@ -18,14 +18,45 @@ package org.seasar.framework.container.factory;
 import org.seasar.framework.container.S2Container;
 
 /**
+ * 
+ * 特定の形式の定義情報から{@link org.seasar.framework.container.S2Container S2コンテナ}を組み立てるビルダのインターフェースです。
+ * 
+ * 
  * @author higa
+ * @author azusa
  * 
  */
 public interface S2ContainerBuilder {
 
+    /**
+     * 指定された設定ファイルからS2コンテナを組み立てます。
+     * 
+     * @param path
+     *            設定ファイルのパス
+     * @return S2コンテナ
+     */
     public S2Container build(String path);
 
+    /**
+     * 指定された設定ファイルから指定されたクラスローダを使用してS2コンテナを組み立てます。
+     * 
+     * @param path
+     *            設定ファイルのパス
+     * @param classLoader
+     *            S2コンテナを組み立てるのに使用するクラスローダ
+     * @return S2コンテナ
+     */
     public S2Container build(String path, ClassLoader classLoader);
 
+    /**
+     * 指定された設定ファイルからS2コンテナを組み立て、親S2コンテナに対してインクルードします。
+     * 
+     * @param parent
+     *            親となるS2コンテナ
+     * @param path
+     *            設定ファイルのパス
+     * @return 構築したS2コンテナ
+     * @see S2Container#include(S2Container)
+     */
     public S2Container include(S2Container parent, String path);
 }
