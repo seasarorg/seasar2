a1ad05b9160f35b5bb5c809ec698aeaf482343d0
nakamura-to <toshihiro.nakamura@gmail.com>
2007-03-16 06:34:01 +0000

    JNDIからEntityManagerFactoryを取得するためのクラスを追加しました。アプリケーションサーバー上ではJNDIから取得したいので。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2123 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/framework/jpa/impl/JndiPersistenceUnitProvider.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.jpa.impl;
+
+import java.util.Map;
+
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+
+import org.seasar.extension.j2ee.JndiResourceLocator;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.framework.container.annotation.tiger.DestroyMethod;
+import org.seasar.framework.container.annotation.tiger.InitMethod;
+import org.seasar.framework.exception.NamingRuntimeException;
+import org.seasar.framework.jpa.PersistenceUnitManager;
+import org.seasar.framework.jpa.PersistenceUnitProvider;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * @author taedium
+ * 
+ */
+public class JndiPersistenceUnitProvider implements PersistenceUnitProvider {
+
+    protected PersistenceUnitManager persistenceUnitManager;
+
+    protected Map<String, String> JndiNames = CollectionsUtil.newHashMap();
+
+    @Binding(bindingType = BindingType.MUST)
+    public void setPersistenceUnitManager(
+            PersistenceUnitManager persistenceUnitManager) {
+        this.persistenceUnitManager = persistenceUnitManager;
+    }
+
+    @InitMethod
+    public void register() {
+        persistenceUnitManager.addProvider(this);
+    }
+
+    @DestroyMethod
+    public void unregister() {
+        persistenceUnitManager.removeProvider(this);
+    }
+
+    public void addJndiName(String unitName, String jndiName) {
+        JndiNames.put(unitName, jndiName);
+    }
+
+    public EntityManagerFactory createEntityManagerFactory(String unitName) {
+        final String name = JndiNames.get(unitName);
+        try {
+            return EntityManagerFactory.class.cast(JndiResourceLocator
+                    .lookup(name));
+        } catch (NamingException e) {
+            throw new NamingRuntimeException(e);
+        }
+    }
+
+}
