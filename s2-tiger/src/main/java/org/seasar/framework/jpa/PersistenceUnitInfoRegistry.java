754e9c2517c810e99729b6db782af76ab4d6c9fb
Koichi Kobayashi <koichik@improvement.jp>
2007-05-02 19:24:19 +0000

    PersistenceUnitInfoRegistry 追加．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2297 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/framework/jpa/PersistenceUnitInfoRegistry.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.jpa;
+
+import javax.persistence.spi.PersistenceUnitInfo;
+
+/**
+ * 永続ユニット情報のレジストリです。
+ * 
+ * @author koichik
+ */
+public interface PersistenceUnitInfoRegistry {
+
+    /**
+     * 名前で指定された永続ユニット情報を返します。
+     * 
+     * @param name
+     *            永続ユニットの名前
+     * @return 名前で指定された永続ユニット情報
+     */
+    PersistenceUnitInfo getPersistenceUnitInfo(String name);
+
+}
---
+++ s2-tiger/src/main/java/org/seasar/framework/jpa/impl/PersistenceUnitInfoRegistryImpl.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.jpa.impl;
+
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
+import org.seasar.framework.container.annotation.tiger.InitMethod;
+import org.seasar.framework.jpa.PersistenceUnitInfoFactory;
+import org.seasar.framework.jpa.PersistenceUnitInfoRegistry;
+import org.seasar.framework.log.Logger;
+import org.seasar.framework.util.ClassLoaderUtil;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * 永続ユニット情報のレジストリです。
+ * 
+ * @author koichik
+ */
+public class PersistenceUnitInfoRegistryImpl implements
+        PersistenceUnitInfoRegistry {
+
+    /** <code>persistence.xml</code>のパス名 */
+    public static final String PERSISTENCE_XML = "META-INF/persistence.xml";
+
+    private static final Logger logger = Logger
+            .getLogger(PersistenceUnitInfoRegistryImpl.class);
+
+    /** 永続ユニット情報のファクトリ */
+    protected PersistenceUnitInfoFactory persistenceUnitInfoFactory;
+
+    /* 永続ユニット名と永続ユニット情報のマップ */
+    protected Map<String, PersistenceUnitInfo> unitInfoMap = CollectionsUtil
+            .newHashMap();
+
+    /**
+     * インスタンスを構築します。
+     */
+    public PersistenceUnitInfoRegistryImpl() {
+    }
+
+    /**
+     * 永続ユニット情報のファクトリを設定します。
+     * 
+     * @param persistenceUnitInfoFactory
+     *            永続ユニット情報のファクトリを設定します。
+     */
+    @Binding(bindingType = BindingType.MUST)
+    public void setPersistenceUnitInfoFactory(
+            final PersistenceUnitInfoFactory persistenceUnitInfoFactory) {
+        this.persistenceUnitInfoFactory = persistenceUnitInfoFactory;
+    }
+
+    /**
+     * <code>META-INF/persistence.xml</code>をロードします。
+     */
+    @InitMethod
+    public void load() {
+        @SuppressWarnings("unchecked")
+        final Iterator<URL> it = ClassLoaderUtil.getResources(PERSISTENCE_XML);
+        while (it.hasNext()) {
+            final URL url = it.next();
+            logger.info(url); // TODO
+            try {
+                for (final PersistenceUnitInfo unitInfo : persistenceUnitInfoFactory
+                        .createPersistenceUnitInfo(it.next())) {
+                    final String unitName = unitInfo.getPersistenceUnitName();
+                    if (!unitInfoMap.containsKey(unitName)) {
+                        logger.info(unitName); // TODO
+                        unitInfoMap.put(unitName, unitInfo);
+                    }
+                }
+            } catch (final Exception e) {
+                logger.error(e); // TODO
+            }
+        }
+    }
+
+    public PersistenceUnitInfo getPersistenceUnitInfo(final String name) {
+        return unitInfoMap.get(name);
+    }
+
+}
