c4a485bc0c43c542f2559c18f9ce1508ac6303ab
vestige <vestige@319488c0-e101-0410-93bc-b5e51f62721a>
2007-06-12 13:42:52 +0000

    javadoc add
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2465 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/AbstractAnnotationHandler.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/factory/AbstractAnnotationHandler.java
@@ -39,34 +39,85 @@ import org.seasar.framework.container.ognl.OgnlExpression;
 import org.seasar.framework.util.ClassUtil;
 import org.seasar.framework.util.StringUtil;
 
+/**
+ * AnnotationHandlerの抽象クラスです。
+ * <p>
+ * このクラスでは、主にクラス、メソッド、フィールドに書かれたアノーテションを取得します。
+ * アノーテションから、コンポーネントに対しての各定義の作成はサブクラスで行います。
+ * </p>
+ * 
+ * @author vestige
+ */
 public abstract class AbstractAnnotationHandler implements AnnotationHandler {
 
+    /**
+     * COMPONENTアノテーションを表す定数名です。
+     */
     protected static final String COMPONENT = "COMPONENT";
 
+    /**
+     * COMPONENTアノテーションのname属性「コンポーネント名」を表すキーです。
+     */
     protected static final String NAME = "name";
 
+    /**
+     * COMPONENTアノテーションのinstance属性「インスタンス定義」を表すキーです。
+     */
     protected static final String INSTANCE = "instance";
 
+    /**
+     * COMPONENTアノテーションのautoBinding属性「自動バインディング定義」を表すキーです。
+     */
     protected static final String AUTO_BINDING = "autoBinding";
 
+    /**
+     * Bindingアノテーションとして識別するための定数です。
+     */
     protected static final String BINDING_SUFFIX = "_BINDING";
 
+    /**
+     * BindingアノテーションのbindingType属性「バインディングタイプ」を表すキーです。
+     */
     protected static final String BINDING_TYPE = "bindingType";
 
+    /**
+     * COMPONENTアノテーションのexternalBinding属性「外部バインディング定義」を表すキーです。
+     */
     protected static final String EXTERNAL_BINDING = "externalBinding";
 
+    /**
+     * 各アノテーション内のvalue属性「VALUE」を表すキーです。
+     */
     protected static final String VALUE = "value";
 
+    /**
+     * ASPECTアノテーションを表す定数名です。
+     */
     protected static final String ASPECT = "ASPECT";
 
+    /**
+     * InterTypeアノテーションを表す定数名です。
+     */
     protected static final String INTER_TYPE = "INTER_TYPE";
 
+    /**
+     * InitMethodアノテーションを表す定数名です。
+     */
     protected static final String INIT_METHOD = "INIT_METHOD";
 
+    /**
+     * DestroyMethodアノテーションを表す定数名です。
+     */
     protected static final String DESTROY_METHOD = "DESTROY_METHOD";
 
+    /**
+     * ASPECTアノテーションのinterceptor属性「インターセプター定義」を表すキーです。
+     */
     protected static final String INTERCEPTOR = "interceptor";
 
+    /**
+     * ASPECTアノテーションのpointcut属性「ポイントカット定義」を表すキーです。
+     */
     protected static final String POINTCUT = "pointcut";
 
     public ComponentDef createComponentDef(String className,
@@ -128,6 +179,18 @@ public abstract class AbstractAnnotationHandler implements AnnotationHandler {
         }
     }
 
+    /**
+     * インスタンス定義を返します。
+     * <p>
+     * 指定した名前のインスタンス定義が存在しない場合、デフォルトのインスタンス定義を返します。
+     * </p>
+     * 
+     * @param name
+     *            インスタンス定義の種類
+     * @param defaultInstanceDef
+     *            デフォルトのインスタンス定義（singleton）
+     * @return インスタンス定義
+     */
     protected InstanceDef getInstanceDef(String name,
             InstanceDef defaultInstanceDef) {
         InstanceDef instanceDef = getInstanceDef(name);
@@ -137,6 +200,13 @@ public abstract class AbstractAnnotationHandler implements AnnotationHandler {
         return defaultInstanceDef;
     }
 
+    /**
+     * インスタンス定義を返します。
+     * 
+     * @param name
+     *            インスタンス定義の種類
+     * @return インスタンス定義
+     */
     protected InstanceDef getInstanceDef(String name) {
         if (StringUtil.isEmpty(name)) {
             return null;
@@ -144,6 +214,13 @@ public abstract class AbstractAnnotationHandler implements AnnotationHandler {
         return InstanceDefFactory.getInstanceDef(name);
     }
 
+    /**
+     * 自動バインディング定義を返します。
+     * 
+     * @param name
+     *            自動バインディング定義の種類
+     * @return 自動バインディング定義
+     */
     protected AutoBindingDef getAutoBindingDef(String name) {
         if (StringUtil.isEmpty(name)) {
             return null;
@@ -151,6 +228,30 @@ public abstract class AbstractAnnotationHandler implements AnnotationHandler {
         return AutoBindingDefFactory.getAutoBindingDef(name);
     }
 
+    /**
+     * 指定したコンポーネントクラスからコンポーネント定義を作成します。
+     * <p>
+     * 以下の設定がある場合はおのおのコンポーネント定義に設定します。
+     * <ul>
+     * <li>コンポーネント名
+     * <li>インスタンス定義
+     * <li>自動バインディング定義
+     * <li>外部バインディングの有無
+     * </ul>
+     * </p>
+     * 
+     * @param componentClass
+     *            コンポーネントクラス
+     * @param name
+     *            コンポーネント名
+     * @param instanceDef
+     *            インスタンス定義
+     * @param autoBindingDef
+     *            自動バインディング定義
+     * @param externalBinding
+     *            外部バインディングの有無
+     * @return コンポーネント定義
+     */
     protected ComponentDef createComponentDef(Class componentClass,
             String name, InstanceDef instanceDef,
             AutoBindingDef autoBindingDef, boolean externalBinding) {
@@ -168,6 +269,19 @@ public abstract class AbstractAnnotationHandler implements AnnotationHandler {
         return componentDef;
     }
 
+    /**
+     * プロパティ定義を作成します。
+     * 
+     * @param propertyName
+     *            プロパティ名
+     * @param expression
+     *            引数定義の値となる式
+     * @param bindingTypeName
+     *            バインディングタイプ定義
+     * @param accessTypeName
+     *            アクセスタイプ定義
+     * @return プロパティ定義
+     */
     protected PropertyDef createPropertyDef(String propertyName,
             String expression, String bindingTypeName, String accessTypeName) {
         PropertyDef propertyDef = new PropertyDefImpl(propertyName);
@@ -216,6 +330,16 @@ public abstract class AbstractAnnotationHandler implements AnnotationHandler {
         return true;
     }
 
+    /**
+     * 指定したフィールドがインジェクション可能かどうかの判定を行います。
+     * <P>
+     * <code>static</code>または、<code>final</code>でない場合はインジェクションが可能になります。
+     * </p>
+     * 
+     * @param field
+     *            フィールド
+     * @return フィールドが<code>static</code>、<code>final</code>でない場合は<code>true</code>、そうでない場合は<code>false</code>を返す。
+     */
     protected boolean isFieldInjectionTarget(Field field) {
         return !Modifier.isStatic(field.getModifiers())
                 && !Modifier.isFinal(field.getModifiers());
