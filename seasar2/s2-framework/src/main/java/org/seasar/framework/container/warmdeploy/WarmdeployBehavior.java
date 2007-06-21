9f25e8b1942e78421c797c05a0de2418670631ec
Koichi Kobayashi <koichik@improvement.jp>
2007-06-21 10:03:18 +0000

    [CONTAINER-44]WARM deploy で複数のスレッドが同じ未登録コンポーネントを同時に要求した場合，それぞれのスレッドでコンポーネントが登録されてしまう問題を修正しました．[Seasar-user:8516]
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2479 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/warmdeploy/WarmdeployBehavior.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/warmdeploy/WarmdeployBehavior.java
@@ -19,11 +19,17 @@ import org.seasar.framework.container.ComponentCreator;
 import org.seasar.framework.container.ComponentDef;
 import org.seasar.framework.container.S2Container;
 import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.container.factory.S2ContainerFactory.Provider;
 import org.seasar.framework.container.impl.S2ContainerBehavior.DefaultProvider;
 import org.seasar.framework.container.util.S2ContainerUtil;
 import org.seasar.framework.convention.NamingConvention;
 import org.seasar.framework.log.Logger;
 
+/**
+ * WARM deploy時にコンポーネントを自動登録する{@link Provider}の実装です。
+ * 
+ * @author higa
+ */
 public class WarmdeployBehavior extends DefaultProvider {
 
     private static final Logger logger = Logger
@@ -33,51 +39,86 @@ public class WarmdeployBehavior extends DefaultProvider {
 
     private ComponentCreator[] creators = new ComponentCreator[0];
 
+    /**
+     * 命名規則を返します。
+     * 
+     * @return 命名規則
+     */
     public NamingConvention getNamingConvention() {
         return namingConvention;
     }
 
+    /**
+     * 命名規則を設定します。
+     * 
+     * @param namingConvention
+     *            命名規則
+     */
     public void setNamingConvention(NamingConvention namingConvention) {
         this.namingConvention = namingConvention;
     }
 
+    /**
+     * コンポーネントクリエータの配列を返します。
+     * 
+     * @return コンポーネントクリエータの配列
+     */
     public ComponentCreator[] getCreators() {
         return creators;
     }
 
+    /**
+     * コンポーネントクリエータの配列を設定します。
+     * 
+     * @param creators
+     *            コンポーネントクリエータの配列
+     */
     public void setCreators(ComponentCreator[] creators) {
         this.creators = creators;
     }
 
     protected ComponentDef getComponentDef(S2Container container, Object key) {
-        ComponentDef cd = super.getComponentDef(container, key);
-        if (cd != null) {
+        synchronized (container.getRoot()) {
+            ComponentDef cd = super.getComponentDef(container, key);
+            if (cd != null) {
+                return cd;
+            }
+            if (container != container.getRoot()) {
+                return null;
+            }
+            if (key instanceof Class) {
+                cd = createComponentDef((Class) key);
+            } else if (key instanceof String) {
+                cd = createComponentDef((String) key);
+                if (cd != null && !key.equals(cd.getComponentName())) {
+                    logger.log("WSSR0011", new Object[] { key,
+                            cd.getComponentClass().getName(),
+                            cd.getComponentName() });
+                    cd = null;
+                }
+            } else {
+                throw new IllegalArgumentException("key");
+            }
+            if (cd != null) {
+                SingletonS2ContainerFactory.getContainer().register(cd);
+                S2ContainerUtil.putRegisterLog(cd);
+                cd.init();
+            }
             return cd;
         }
-        if (container != container.getRoot()) {
-            return null;
-        }
-        if (key instanceof Class) {
-            cd = createComponentDef((Class) key);
-        } else if (key instanceof String) {
-            cd = createComponentDef((String) key);
-            if (cd != null && !key.equals(cd.getComponentName())) {
-                logger.log("WSSR0011",
-                        new Object[] { key, cd.getComponentClass().getName(),
-                                cd.getComponentName() });
-                cd = null;
-            }
-        } else {
-            throw new IllegalArgumentException("key");
-        }
-        if (cd != null) {
-            SingletonS2ContainerFactory.getContainer().register(cd);
-            S2ContainerUtil.putRegisterLog(cd);
-            cd.init();
-        }
-        return cd;
     }
 
+    /**
+     * コンポーネント定義を作成します。
+     * <p>
+     * コンポーネントクリエータを順次呼び出し、コンポーネント定義が作成された場合はそれを返します。
+     * どのコンポーネントクリエータからもコンポーネント定義が作成されなかった場合は<code>null</code>を返します。
+     * </p>
+     * 
+     * @param componentClass
+     *            コンポーネントのクラス
+     * @return コンポーネント定義
+     */
     protected ComponentDef createComponentDef(Class componentClass) {
         for (int i = 0; i < creators.length; ++i) {
             ComponentCreator creator = creators[i];
@@ -89,6 +130,17 @@ public class WarmdeployBehavior extends DefaultProvider {
         return null;
     }
 
+    /**
+     * コンポーネント定義を作成します。
+     * <p>
+     * コンポーネントクリエータを順次呼び出し、コンポーネント定義が作成された場合はそれを返します。
+     * どのコンポーネントクリエータからもコンポーネント定義が作成されなかった場合は<code>null</code>を返します。
+     * </p>
+     * 
+     * @param componentName
+     *            コンポーネント名
+     * @return コンポーネント定義
+     */
     protected ComponentDef createComponentDef(String componentName) {
         for (int i = 0; i < creators.length; ++i) {
             ComponentCreator creator = creators[i];
@@ -99,4 +151,5 @@ public class WarmdeployBehavior extends DefaultProvider {
         }
         return null;
     }
+
 }
