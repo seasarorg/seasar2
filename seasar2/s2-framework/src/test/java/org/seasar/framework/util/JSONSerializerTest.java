e55cbecfc651cf233f1f8826a5ee3fa3f93a2dcd
Koichi Kobayashi <koichik@improvement.jp>
2007-07-22 13:05:31 +0000

    Javadoc の警告が出ないようにしました．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2653 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/test/java/org/seasar/framework/util/JSONSerializerTest.java
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/util/JSONSerializerTest.java
@@ -23,23 +23,36 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 /**
- * @author mopemope
+ * {@link JSONSerializer}のテストクラスです。
  * 
+ * @author mopemope
  */
 public class JSONSerializerTest extends TestCase {
+    /**
+     * @throws Exception
+     */
     public void testToString_null() throws Exception {
         assertEquals("null", JSONSerializer.serialize(null));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_boolean() throws Exception {
         assertEquals("true", JSONSerializer.serialize(Boolean.TRUE));
         assertEquals("false", JSONSerializer.serialize(Boolean.FALSE));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_string() throws Exception {
         assertEquals("\"a\"", JSONSerializer.serialize("a"));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_float() throws Exception {
         assertEquals("1.0", JSONSerializer.serialize(new Float(1.0f)));
         try {
@@ -50,6 +63,9 @@ public class JSONSerializerTest extends TestCase {
         }
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_double() throws Exception {
         assertEquals("1.0", JSONSerializer.serialize(new Double(1.0d)));
         try {
@@ -60,10 +76,16 @@ public class JSONSerializerTest extends TestCase {
         }
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_number() throws Exception {
         assertEquals("100", JSONSerializer.serialize(new Integer(100)));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_array() throws Exception {
         assertEquals("[]", JSONSerializer.serialize(new Object[0]));
         assertEquals("[1]", JSONSerializer
@@ -72,6 +94,9 @@ public class JSONSerializerTest extends TestCase {
                 new Integer(1), new Integer(2) }));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_collection() throws Exception {
         assertEquals("[]", JSONSerializer.serialize(new ArrayList()));
         ArrayList list = new ArrayList();
@@ -79,6 +104,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals("[1]", JSONSerializer.serialize(list));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_map() throws Exception {
         assertEquals("{}", JSONSerializer.serialize(new HashMap()));
         HashMap map = new HashMap();
@@ -90,6 +118,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals("{aaa:1,bbb:{}}", JSONSerializer.serialize(map));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_PrimitiveArray() throws Exception {
         assertEquals("[1]", JSONSerializer.serialize(new int[] { 1 }));
         assertEquals("[1,2,3]", JSONSerializer.serialize(new int[] { 1, 2, 3 }));
@@ -100,12 +131,15 @@ public class JSONSerializerTest extends TestCase {
         assertEquals("[1.11,2.22,3.33]", JSONSerializer.serialize(new double[] {
                 1.11, 2.22, 3.33 }));
 
-//        assertEquals(
-//                "{bbb:[11,22,33],ccc:[true,false,true],ddd:[1.11,2.22,3.33]}",
-//                JSONSerializer.serialize(new Test2()));
-      System.out.println(JSONSerializer.serialize(new Test2()));
+        // assertEquals(
+        // "{bbb:[11,22,33],ccc:[true,false,true],ddd:[1.11,2.22,3.33]}",
+        // JSONSerializer.serialize(new Test2()));
+        System.out.println(JSONSerializer.serialize(new Test2()));
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_bean() throws Exception {
         String s = JSONSerializer.serialize(new MyBean());
         System.out.println(s);
@@ -118,6 +152,9 @@ public class JSONSerializerTest extends TestCase {
         System.out.println(s);
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_htmlBean1() throws Exception {
         final HtmlBean bean = new HtmlBean();
         final String s = JSONSerializer.serialize(bean);
@@ -125,6 +162,9 @@ public class JSONSerializerTest extends TestCase {
         assertTrue(s.indexOf("html:null") > 0);
     }
 
+    /**
+     * @throws Exception
+     */
     public void testToJson_htmlBean2() throws Exception {
         final HtmlBean bean = new HtmlBean();
         bean.setStatus(new Integer(30));
@@ -134,6 +174,9 @@ public class JSONSerializerTest extends TestCase {
         assertTrue(s.indexOf("html:\"<div>hoge<\\/div>\"") > 0);
     }
 
+    /**
+     * @throws Exception
+     */
     public void testQuote() throws Exception {
         assertEquals("\"a\"", JSONSerializer.quote("a"));
         assertEquals("\"\\t\"", JSONSerializer.quote("\t"));
@@ -144,54 +187,81 @@ public class JSONSerializerTest extends TestCase {
         assertEquals("\"\\/\"", JSONSerializer.quote("/"));
     }
 
+    /**
+     * 
+     */
     public void testIsObject1() {
         String str = "{'test1':'test2'}";
         boolean result = JSONSerializer.isObject(str);
         assertTrue(result);
     }
 
+    /**
+     * 
+     */
     public void testIsObject2() {
         String str = "['test1','test2']";
         boolean result = JSONSerializer.isObject(str);
         assertFalse(result);
     }
 
+    /**
+     * 
+     */
     public void testIsObject3() {
         String str = "'test2'";
         boolean result = JSONSerializer.isObject(str);
         assertFalse(result);
     }
 
+    /**
+     * 
+     */
     public void testIsArray1() {
         String str = "['test1','test2']";
         boolean result = JSONSerializer.isArray(str);
         assertTrue(result);
     }
 
+    /**
+     * 
+     */
     public void testIsArray2() {
         String str = "{'test1','test2'}";
         boolean result = JSONSerializer.isArray(str);
         assertFalse(result);
     }
 
+    /**
+     * 
+     */
     public void testIsArray3() {
         String str = "'test2'";
         boolean result = JSONSerializer.isArray(str);
         assertFalse(result);
     }
 
+    /**
+     * 
+     */
     public void testParseObject1() {
         String str = "{'test1':'test2'}";
         Map map = JSONSerializer.evalMap(str);
         assertEquals("{test1=test2}", map.toString());
     }
 
+    /**
+     * 
+     */
     public void testParseObject2() {
         String str = "{'test1':test2}";
         Map map = JSONSerializer.evalMap(str);
         assertEquals("{test1=test2}", map.toString());
     }
 
+    /**
+     * 
+     */
     public void testParseObject3() {
         String str = "{'test1':3}";
         Map map = JSONSerializer.evalMap(str);
@@ -199,6 +269,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals(i, new Integer(3));
     }
 
+    /**
+     * 
+     */
     public void testParseObject4() {
         String str = "{'test1':'4'}";
         Map map = JSONSerializer.evalMap(str);
@@ -206,6 +279,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals(i, "4");
     }
 
+    /**
+     * 
+     */
     public void testParseObject5() {
         String str = "{'test2':{'test1':'5'}}";
         Map map = JSONSerializer.evalMap(str);
@@ -215,18 +291,27 @@ public class JSONSerializerTest extends TestCase {
         assertEquals(s, "5");
     }
 
+    /**
+     * 
+     */
     public void testParseObject6() {
         String str = "{'test1':{'test2':'2'},'test3':{'test4':'4'}}";
         Map map = JSONSerializer.evalMap(str);
         assertEquals("{test3={test4=4}, test1={test2=2}}", map.toString());
     }
 
+    /**
+     * 
+     */
     public void testParseArray1() {
         String str = "['test1']";
         List list = JSONSerializer.evalArray(str);
         assertEquals("test1", list.get(0));
     }
 
+    /**
+     * 
+     */
     public void testParseArray2() {
         String str = "['test1', 'test2']";
         List list = JSONSerializer.evalArray(str);
@@ -234,6 +319,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals("test2", list.get(1));
     }
 
+    /**
+     * 
+     */
     public void testParseArray3() {
         String str = "[1, 2]";
         List list = JSONSerializer.evalArray(str);
@@ -241,6 +329,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals(new Integer(2), list.get(1));
     }
 
+    /**
+     * 
+     */
     public void testParseArray4() {
         String str = "[1, [2, 3, 4, 5], 3]";
         List list = JSONSerializer.evalArray(str);
@@ -248,6 +339,9 @@ public class JSONSerializerTest extends TestCase {
         assertEquals(list.toString(), "[1, [2, 3, 4, 5], 3]");
     }
 
+    /**
+     * 
+     */
     public void testParseArray5() {
         String str = "[{'test1':1}, {'test2':'2'}]";
         List list = JSONSerializer.evalArray(str);
@@ -259,34 +353,55 @@ public class JSONSerializerTest extends TestCase {
         assertEquals(s, "2");
     }
 
+    /**
+     * 
+     */
     public void testParseArray6() {
         String str = "[{'test1':{'test2':'2'},'test3':{'test4':'4'}}]";
         List list = JSONSerializer.evalArray(str);
         assertEquals("[{test3={test4=4}, test1={test2=2}}]", list.toString());
     }
 
+    /**
+     * @author mopemope
+     */
     public static class MyBean {
         private String aaa;
 
         private MyBean bbb;
 
+        /**
+         * @return
+         */
         public String getAaa() {
             return aaa;
         }
 
+        /**
+         * @param aaa
+         */
         public void setAaa(String aaa) {
             this.aaa = aaa;
         }
 
+        /**
+         * @return
+         */
         public MyBean getBbb() {
             return bbb;
         }
 
+        /**
+         * @param bbb
+         */
         public void setBbb(MyBean bbb) {
             this.bbb = bbb;
         }
     }
 
+    /**
+     * @author mopemope
+     */
     public static class Test1 {
         private String aaa = "hoge";
 
@@ -296,23 +411,38 @@ public class JSONSerializerTest extends TestCase {
 
         private double ddd = 3.14;
 
+        /**
+         * @return
+         */
         public String getAaa() {
             return aaa;
         }
 
+        /**
+         * @return
+         */
         public int getBbb() {
             return bbb;
         }
 
+        /**
+         * @return
+         */
         public boolean isCcc() {
             return ccc;
         }
 
+        /**
+         * @return
+         */
         public double getDdd() {
             return ddd;
         }
     }
 
+    /**
+     * @author mopemope
+     */
     public static class Test2 {
 
         private int[] bbb = { 11, 22, 33 };
@@ -321,37 +451,61 @@ public class JSONSerializerTest extends TestCase {
 
         private double[] ddd = { 1.11, 2.22, 3.33 };
 
+        /**
+         * @return
+         */
         public int[] getBbb() {
             return bbb;
         }
 
+        /**
+         * @return
+         */
         public boolean[] getCcc() {
             return ccc;
         }
 
+        /**
+         * @return
+         */
         public double[] getDdd() {
             return ddd;
         }
     }
 
+    /**
+     * @author mopemope
+     */
     public static class HtmlBean {
 
         private Integer status;
 
         private String html;
 
+        /**
+         * @return
+         */
         public String getHtml() {
             return html;
         }
 
+        /**
+         * @param html
+         */
         public void setHtml(final String html) {
             this.html = html;
         }
 
+        /**
+         * @return
+         */
         public Integer getStatus() {
             return status;
         }
 
+        /**
+         * @param status
+         */
         public void setStatus(final Integer status) {
             this.status = status;
         }
