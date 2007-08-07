809fc8a246e05502bcce03825d755b8de7588db9
Koichi Kobayashi <koichik@improvement.jp>
2007-08-07 13:50:00 +0000

    [CONTAINER-72] MessageResourceFactory の getBundle(String, Locale) および getNullableBundle(String, Locale) において，Locale の言語コードだけでなく国コードおよびバリアントコードも含めてリソースを検索するようにしました．[Seasar-user:9753] ＰＰＯＩさんからパッチを頂きました．ありがとうございます．
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2689 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/message/MessageResourceBundleFactory.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/message/MessageResourceBundleFactory.java
@@ -22,6 +22,8 @@ import java.util.Map;
 
 import org.seasar.framework.exception.ResourceNotFoundRuntimeException;
 import org.seasar.framework.util.AssertionUtil;
+import org.seasar.framework.util.Disposable;
+import org.seasar.framework.util.DisposableUtil;
 import org.seasar.framework.util.ResourceUtil;
 
 /**
@@ -33,7 +35,11 @@ public class MessageResourceBundleFactory {
 
     private static final String PROPERTIES_EXT = ".properties";
 
+    private static final Object NOT_FOUND = new Object();
+
     private static Map cache = new HashMap();
+
+    private static boolean initialized = false;
 
     /**
      * {@link MessageResourceBundle}を返します。
@@ -88,19 +94,26 @@ public class MessageResourceBundleFactory {
         AssertionUtil.assertNotNull("locale", locale);
 
         String base = baseName.replace('.', '/');
-        String path = base + "_" + locale.getLanguage() + PROPERTIES_EXT;
-        MessageResourceBundleFacade facade = loadFacade(path);
-        String parentPath = base + PROPERTIES_EXT;
-        MessageResourceBundleFacade parentFacade = loadFacade(parentPath);
-        if (facade != null && parentFacade != null) {
-            facade.setParent(parentFacade);
-            return facade.getBundle();
-        } else if (facade != null) {
-            return facade.getBundle();
-        } else if (parentFacade != null) {
-            return parentFacade.getBundle();
+
+        String[] bundleNames = calcurateBundleNames(base, locale);
+        MessageResourceBundleFacade parentFacade = null;
+        MessageResourceBundleFacade facade = null;
+        int length = bundleNames.length;
+        for (int i = 0; i < length; ++i) {
+            facade = loadFacade(bundleNames[i] + PROPERTIES_EXT);
+            if (parentFacade == null) {
+                parentFacade = facade;
+            } else if (facade != null) {
+                facade.setParent(parentFacade);
+                parentFacade = facade;
+            }
         }
-        return null;
+
+        if (parentFacade != null) {
+            return parentFacade.getBundle();
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -112,19 +125,90 @@ public class MessageResourceBundleFactory {
      */
     protected static MessageResourceBundleFacade loadFacade(String path) {
         synchronized (cache) {
-            MessageResourceBundleFacade facade = (MessageResourceBundleFacade) cache
-                    .get(path);
-            if (facade != null) {
-                return facade;
+            if (!initialized) {
+                DisposableUtil.add(new Disposable() {
+                    public void dispose() {
+                        clear();
+                        initialized = false;
+                    }
+                });
+                initialized = true;
+            }
+            Object cachedFacade = cache.get(path);
+            if (cachedFacade == NOT_FOUND) {
+                return null;
+            } else if (cachedFacade != null) {
+                return (MessageResourceBundleFacade) cachedFacade;
             }
             URL url = ResourceUtil.getResourceNoException(path);
             if (url != null) {
-                facade = new MessageResourceBundleFacade(url);
+                MessageResourceBundleFacade facade = new MessageResourceBundleFacade(
+                        url);
                 cache.put(path, facade);
                 return facade;
+            } else {
+                cache.put(path, NOT_FOUND);
             }
         }
         return null;
+    }
+
+    /**
+     * リソースバンドル名の配列をロケールから求めて返します。
+     * 
+     * @param baseName
+     *            リソースバンドルの基底名
+     * @param locale
+     *            リソースバンドルが必要なロケール
+     * @return リソースバンドル名配列
+     */
+    protected static String[] calcurateBundleNames(String baseName,
+            Locale locale) {
+        int length = 1;
+        boolean l = locale.getLanguage().length() > 0;
+        if (l) {
+            length++;
+        }
+        boolean c = locale.getCountry().length() > 0;
+        if (c) {
+            length++;
+        }
+        boolean v = locale.getVariant().length() > 0;
+        if (v) {
+            length++;
+        }
+        String[] result = new String[length];
+        int index = 0;
+        result[index++] = baseName;
+
+        if (!(l || c || v)) {
+            return result;
+        }
+
+        StringBuffer buffer = new StringBuffer(baseName);
+        buffer.append('_');
+        buffer.append(locale.getLanguage());
+        if (l) {
+            result[index++] = new String(buffer);
+        }
+
+        if (!(c || v)) {
+            return result;
+        }
+        buffer.append('_');
+        buffer.append(locale.getCountry());
+        if (c) {
+            result[index++] = new String(buffer);
+        }
+
+        if (!v) {
+            return result;
+        }
+        buffer.append('_');
+        buffer.append(locale.getVariant());
+        result[index++] = new String(buffer);
+
+        return result;
     }
 
     /**
--- seasar2/s2-framework/src/test/java/org/seasar/framework/message/MessageResourceBundleFactoryTest.java
+++ seasar2/s2-framework/src/test/java/org/seasar/framework/message/MessageResourceBundleFactoryTest.java
@@ -15,6 +15,7 @@
  */
 package org.seasar.framework.message;
 
+import java.util.List;
 import java.util.Locale;
 
 import junit.framework.TestCase;
@@ -58,5 +59,104 @@ public class MessageResourceBundleFactoryTest extends TestCase {
         assertNotNull(bundle);
         assertNull(bundle.getParent());
         assertEquals("{0} not found", bundle.get("ESSR0001"));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetNullabelBundle2() throws Exception {
+        String baseName = "org.seasar.framework.message.strings";
+        MessageResourceBundle bundle = MessageResourceBundleFactory
+                .getNullableBundle(baseName, Locale.JAPANESE);
+        assertNotNull(bundle);
+        assertNotNull(bundle.getParent());
+        assertNull(bundle.getParent().getParent());
+        assertEquals("hogehoge", bundle.get("text"));
+
+        bundle = MessageResourceBundleFactory.getNullableBundle(baseName,
+                new Locale("ja", "JP"));
+        assertNotNull(bundle);
+        assertNotNull(bundle.getParent());
+        assertNotNull(bundle.getParent().getParent());
+        assertNull(bundle.getParent().getParent().getParent());
+        assertEquals("hogehogehoge", bundle.get("text"));
+
+        bundle = MessageResourceBundleFactory.getNullableBundle(baseName,
+                new Locale("ja", "JP", "WIN"));
+        assertNotNull(bundle);
+        assertNotNull(bundle.getParent());
+        assertNotNull(bundle.getParent().getParent());
+        assertNotNull(bundle.getParent().getParent().getParent());
+        assertNull(bundle.getParent().getParent().getParent().getParent());
+        assertEquals("hogehogehogehoge", bundle.get("text"));
+
+        bundle = MessageResourceBundleFactory.getNullableBundle(baseName,
+                new Locale("en", "US"));
+        assertNotNull(bundle);
+        assertNotNull(bundle.getParent());
+        assertNotNull(bundle.getParent().getParent());
+        assertNull(bundle.getParent().getParent().getParent());
+        assertEquals("foobar", bundle.get("text"));
+
+        bundle = MessageResourceBundleFactory.getNullableBundle(baseName,
+                new Locale("en", "UK"));
+        assertNotNull(bundle);
+        assertNotNull(bundle.getParent());
+        assertNull(bundle.getParent().getParent());
+        assertEquals("foo", bundle.get("text"));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testCalcurateBundleNames() throws Exception {
+        // 言語のみ
+        String[] bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, Locale.JAPANESE);
+        List expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "_ja"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
+
+        // 言語と国
+        bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, new Locale("ja", "JP"));
+        expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "_ja", BASE_NAME + "_ja_JP"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
+
+        // 言語と国とバリアント
+        bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, new Locale("ja", "JP", "WIN"));
+        expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "_ja", BASE_NAME + "_ja_JP", BASE_NAME + "_ja_JP_WIN"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
+
+        // 言語とバリアント
+        bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, new Locale("ja", "", "WIN"));
+        expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "_ja", BASE_NAME + "_ja__WIN"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
+
+        // 国とバリアント
+        bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, new Locale("", "JP", "WIN"));
+        expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "__JP", BASE_NAME + "__JP_WIN"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
+
+        // 国のみ
+        bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, new Locale("", "JP"));
+        expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "__JP"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
+
+        // バリアントのみ
+        bundleNames = MessageResourceBundleFactory
+                .calcurateBundleNames(BASE_NAME, new Locale("", "", "WIN"));
+        expected = java.util.Arrays.asList( new String[]{BASE_NAME,
+                BASE_NAME + "___WIN"} );
+        assertEquals( expected, java.util.Arrays.asList(bundleNames) );
     }
 }
