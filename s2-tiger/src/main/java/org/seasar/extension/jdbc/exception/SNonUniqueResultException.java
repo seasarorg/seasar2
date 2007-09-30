67fa6f0279c5afc5037e82403c42e5e9afd40c12
Yasuo Higa <higayasuo@gmail.com>
2007-09-30 09:21:57 +0000

    NonUniqueResultRuntimeExceptionをJPA用に変更。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@2801 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/extension/jdbc/exception/SNonUniqueResultException.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jdbc.exception;
+
+import javax.persistence.NonUniqueResultException;
+
+import org.seasar.framework.message.MessageFormatter;
+
+/**
+ * <p>
+ * クエリの結果がユニークでない場合の例外です。 {@link NonUniqueResultException}のSeasar2拡張です。
+ * </p>
+ * 
+ * @author higa
+ * 
+ */
+public final class SNonUniqueResultException extends NonUniqueResultException {
+
+    private static final long serialVersionUID = 0L;
+
+    private String sql;
+
+    /**
+     * {@link SNonUniqueResultException}を作成します。
+     * 
+     * @param sql
+     *            SQL
+     */
+    public SNonUniqueResultException(String sql) {
+        super(MessageFormatter.getMessage("ESSR0326", new Object[] { sql }));
+        this.sql = sql;
+    }
+
+    /**
+     * SQLを返します。
+     * 
+     * @return SQL
+     */
+    public String getSql() {
+        return sql;
+    }
+}
