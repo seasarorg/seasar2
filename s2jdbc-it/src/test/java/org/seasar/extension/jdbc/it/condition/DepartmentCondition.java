4e8bfa5b5e66ce35065b88b19975907fdd6f1b37
Koichi Kobayashi <koichik@improvement.jp>
2007-10-26 09:55:00 +0000

    r:3023 でなぜかファイルが 2 つばかりコミットから漏れてしまったので追加でコミット．
    http://d.hatena.ne.jp/mopemope/20071026/p1
    を受けて案を考えてみました．
    s2jdbc-it プロジェクトの conditon パッケージに追加した EmployeeCondition の様なクラスを用意することで，
    
    .from(Employee.class)
    .where(new EmployeeCondition().employeeName.eq("SMITH"))
    
    のようにプロパティ名を文字列で書かなくてもすみます．
    関連を使った場合は
    
    .from(Department.class)
    .join("employees")
    .join("employees.address")
    .where(new DepartmentCondition().employees().addressId.eq(3))
    
    のようになります．関連はフィールドじゃなくてメソッドになってるのがカッコ悪いですが．．． 無念だ．
    s2jdbc-it にある AutoSelectTest の test～Condition() をサンプル代わりに参照してください．
    最終的には Dolteng 等による Condition の自動生成ができないと実用にはなりませんが，それもいいんじゃないのかな，と．
    
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3024 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2jdbc-it/src/test/java/org/seasar/extension/jdbc/it/condition/DepartmentCondition.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jdbc.it.condition;
+
+import org.seasar.extension.jdbc.where.condition.AbstractEntityCondition;
+import org.seasar.extension.jdbc.where.condition.NotNullableCondition;
+import org.seasar.extension.jdbc.where.condition.NullableCondition;
+import org.seasar.extension.jdbc.where.condition.NullableStringCondition;
+
+/**
+ * @author koichik
+ */
+public class DepartmentCondition extends
+        AbstractEntityCondition<DepartmentCondition> {
+
+    /**
+     * 
+     */
+    public DepartmentCondition() {
+    }
+
+    /**
+     * @param prefix
+     */
+    public DepartmentCondition(String prefix) {
+        super(prefix);
+    }
+
+    /** */
+    public NotNullableCondition<DepartmentCondition, Integer> departmentId =
+        new NotNullableCondition<DepartmentCondition, Integer>(
+            "departmentId",
+            this);
+
+    /** */
+    public NullableCondition<DepartmentCondition, Integer> departmentNo =
+        new NullableCondition<DepartmentCondition, Integer>(
+            "departmentNo",
+            this);
+
+    /** */
+    public NullableStringCondition<DepartmentCondition> departmentName =
+        new NullableStringCondition<DepartmentCondition>("departmentName", this);
+
+    /** */
+    public NullableStringCondition<DepartmentCondition> location =
+        new NullableStringCondition<DepartmentCondition>("location", this);
+
+    /** */
+    public NullableCondition<DepartmentCondition, Integer> version =
+        new NullableCondition<DepartmentCondition, Integer>("version", this);
+
+    /**
+     * @return
+     */
+    public EmployeeCondition employees() {
+        return new EmployeeCondition(prefix + "employees.");
+    }
+
+}
