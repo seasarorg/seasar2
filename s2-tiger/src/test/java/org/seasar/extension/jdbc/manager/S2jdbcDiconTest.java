5e183e970ecbe873d49bf308f8ebfda7d90d4ca7
Yasuo Higa <higayasuo@gmail.com>
2007-10-28 04:25:42 +0000

    jdbcManager.selecBySql()のMapを使ったテストを追加。 List<RowMap> results = jdbcManager.selectBySql(RowMap.class,...).getResultList(); で書いているけど、本当は、 List<Map<String, Object>> results = jdbcManager.selectBySql(Map<String, Object>.class,...).getResultList(); 見たいな感じで書きたい。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3040 319488c0-e101-0410-93bc-b5e51f62721a

--- s2-tiger/src/test/java/org/seasar/extension/jdbc/manager/S2jdbcDiconTest.java
+++ s2-tiger/src/test/java/org/seasar/extension/jdbc/manager/S2jdbcDiconTest.java
@@ -1,7 +1,10 @@
 package org.seasar.extension.jdbc.manager;
 
+import java.util.List;
+
 import org.seasar.extension.jdbc.JdbcManager;
 import org.seasar.extension.jdbc.entity.Emp;
+import org.seasar.extension.jdbc.util.RowMap;
 import org.seasar.extension.unit.S2TestCase;
 
 /**
@@ -22,5 +25,15 @@ public class S2jdbcDiconTest extends S2TestCase {
      */
     public void testGetResultList() throws Exception {
         assertTrue(jdbcManager.from(Emp.class).getResultList().size() > 1);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetResultList_map() throws Exception {
+        List<RowMap> results = jdbcManager.selectBySql(RowMap.class,
+                "select * from emp").getResultList();
+        assertNotNull(results);
+
     }
 }
