bc9ed27e0e4dc004ab961622e7d809a5cc1727a1
Yasuo Higa <higayasuo@gmail.com>
2007-11-06 00:36:05 +0000

    更新用のサンプルを追加しました。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3140 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2jdbc-tutorial/src/test/java/examples/entity/UpdateTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.entity;
+
+import org.seasar.extension.jdbc.JdbcManager;
+import org.seasar.extension.unit.S2TestCase;
+
+/**
+ * @author higa
+ * 
+ */
+public class UpdateTest extends S2TestCase {
+
+    private JdbcManager jdbcManager;
+
+    protected void setUp() throws Exception {
+        include("app.dicon");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testUpdateTx() throws Exception {
+        Employee emp =
+            jdbcManager
+                .from(Employee.class)
+                .where("id = ?", 1)
+                .getSingleResult();
+        emp.name = "hoge";
+        System.out.println(emp.version);
+        jdbcManager.update(emp).execute();
+        System.out.println(emp.version);
+    }
+}
