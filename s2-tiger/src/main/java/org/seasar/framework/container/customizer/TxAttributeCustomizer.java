e307dd15723fe6fea8fe1272578f167f66998fb7
Koichi Kobayashi <koichik@improvement.jp>
2007-11-10 11:59:33 +0000

    [CONTAINER-115] EJB3 の @TransactionAttribute アノテーションを POJO (非 EJB) に指定してトランザクション属性を設定可能にする TxAttributeCustomizer を追加しました． https://www.seasar.org/issues/browse/CONTAINER-115
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3190 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/framework/container/customizer/TxAttributeCustomizer.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.customizer;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.factory.AspectDefFactory;
+import org.seasar.framework.util.StringUtil;
+import org.seasar.framework.util.tiger.CollectionsUtil;
+
+/**
+ * @author koichik
+ */
+public class TxAttributeCustomizer extends AbstractCustomizer {
+
+    /** トランザクション属性に対応するインターセプタ名の<code>Map</code> */
+    protected static final Map<TransactionAttributeType, String> txInterceptors = CollectionsUtil
+            .newHashMap();
+    static {
+        txInterceptors.put(TransactionAttributeType.MANDATORY,
+                "j2ee.mandatoryTx");
+        txInterceptors
+                .put(TransactionAttributeType.REQUIRED, "j2ee.requiredTx");
+        txInterceptors.put(TransactionAttributeType.REQUIRES_NEW,
+                "j2ee.requiresNewTx");
+        txInterceptors.put(TransactionAttributeType.NOT_SUPPORTED,
+                "j2ee.notSupportedTx");
+        txInterceptors.put(TransactionAttributeType.NEVER, "j2ee.neverTx");
+    }
+
+    /** デフォルトのトランザクション属性 */
+    protected TransactionAttributeType defaultAttributeType = TransactionAttributeType.REQUIRED;
+
+    /**
+     * インスタンスを構築します。
+     */
+    public TxAttributeCustomizer() {
+    }
+
+    /**
+     * デフォルトのトランザクション属性を設定します。
+     * 
+     * @param defaultAttributeType
+     *            デフォルトのトランザクション属性
+     */
+    public void setDefaultAttributeType(
+            final TransactionAttributeType defaultAttributeType) {
+        this.defaultAttributeType = defaultAttributeType;
+    }
+
+    @Override
+    protected void doCustomize(final ComponentDef componentDef) {
+        final Class<?> componentClass = componentDef.getComponentClass();
+        final TransactionAttribute classAttribute = componentClass
+                .getAnnotation(TransactionAttribute.class);
+        final TransactionAttributeType classAttributeType = classAttribute != null ? classAttribute
+                .value()
+                : defaultAttributeType;
+        for (final Method method : componentClass.getMethods()) {
+            if (method.isSynthetic() || method.isBridge()) {
+                continue;
+            }
+            if (method.getDeclaringClass() == Object.class) {
+                continue;
+            }
+            final TransactionAttribute methodAttribute = method
+                    .getAnnotation(TransactionAttribute.class);
+            final TransactionAttributeType methodAttributeType = methodAttribute != null ? methodAttribute
+                    .value()
+                    : classAttributeType;
+            final String interceptorName = txInterceptors
+                    .get(methodAttributeType);
+            if (!StringUtil.isEmpty(interceptorName)) {
+                componentDef.addAspectDef(AspectDefFactory.createAspectDef(
+                        interceptorName, method));
+            }
+        }
+    }
+
+}
---
+++ s2-tiger/src/test/java/org/seasar/framework/container/customizer/TxAttributeCustomizerTest.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.framework.container.customizer;
+
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.seasar.extension.tx.MandatoryInterceptor;
+import org.seasar.extension.tx.RequiredInterceptor;
+import org.seasar.extension.tx.RequiresNewInterceptor;
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.framework.container.AspectDef;
+import org.seasar.framework.container.ComponentDef;
+
+/**
+ * @author koichik
+ */
+public class TxAttributeCustomizerTest extends S2TestCase {
+
+    /**
+     * @throws Exception
+     */
+    @Override
+    public void setUp() throws Exception {
+        include("TxAttributeCustomizerTest.dicon");
+    }
+
+    /**
+     */
+    public void testCustomize1() {
+        ComponentDef cd = getComponentDef(Comp1.class);
+        assertEquals(1, cd.getAspectDefSize());
+
+        AspectDef ad = cd.getAspectDef(0);
+        assertEquals(RequiredInterceptor.class, ad.getAspect()
+                .getMethodInterceptor().getClass());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testCustomize2() throws Exception {
+        ComponentDef cd = getComponentDef(Comp2.class);
+        assertEquals(2, cd.getAspectDefSize());
+
+        AspectDef ad = cd.getAspectDef(0);
+        assertEquals(RequiredInterceptor.class, ad.getAspect()
+                .getMethodInterceptor().getClass());
+        assertTrue(ad.getPointcut().isApplied(
+                Comp2.class.getMethod("foo", null)));
+
+        ad = cd.getAspectDef(1);
+        assertEquals(RequiresNewInterceptor.class, ad.getAspect()
+                .getMethodInterceptor().getClass());
+        assertTrue(ad.getPointcut().isApplied(
+                Comp2.class.getMethod("bar", null)));
+    }
+
+    /**
+     */
+    public void testCustomize3() {
+        ComponentDef cd = getComponentDef(Comp3.class);
+        assertEquals(1, cd.getAspectDefSize());
+
+        AspectDef ad = cd.getAspectDef(0);
+        assertEquals(MandatoryInterceptor.class, ad.getAspect()
+                .getMethodInterceptor().getClass());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testCustomize4() throws Exception {
+        ComponentDef cd = getComponentDef(Comp4.class);
+        assertEquals(2, cd.getAspectDefSize());
+
+        AspectDef ad = cd.getAspectDef(0);
+        assertEquals(MandatoryInterceptor.class, ad.getAspect()
+                .getMethodInterceptor().getClass());
+        assertTrue(ad.getPointcut().isApplied(
+                Comp4.class.getMethod("foo", null)));
+
+        ad = cd.getAspectDef(1);
+        assertEquals(RequiresNewInterceptor.class, ad.getAspect()
+                .getMethodInterceptor().getClass());
+        assertTrue(ad.getPointcut().isApplied(
+                Comp4.class.getMethod("bar", null)));
+    }
+
+    /**
+     * @author koichik
+     */
+    public static class Comp1 {
+
+        /**
+         * 
+         */
+        public void foo() {
+        }
+
+    }
+
+    /**
+     * @author koichik
+     */
+    public static class Comp2 {
+
+        /**
+         * 
+         */
+        public void foo() {
+        }
+
+        /**
+         * 
+         */
+        @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+        public void bar() {
+        }
+
+    }
+
+    /**
+     * @author koichik
+     */
+    @TransactionAttribute(TransactionAttributeType.MANDATORY)
+    public static class Comp3 {
+
+        /**
+         * 
+         */
+        public void foo() {
+        }
+
+    }
+
+    /**
+     * @author koichik
+     */
+    @TransactionAttribute(TransactionAttributeType.MANDATORY)
+    public static class Comp4 {
+
+        /**
+         * 
+         */
+        public void foo() {
+        }
+
+        /**
+         * 
+         */
+        @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+        public void bar() {
+        }
+
+    }
+
+}
