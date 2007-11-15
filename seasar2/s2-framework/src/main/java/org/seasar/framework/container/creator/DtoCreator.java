9bbd3bd245c4e93b9d2ff0d4659603d3060d68fd
yatsu <yatsu@319488c0-e101-0410-93bc-b5e51f62721a>
2007-11-15 12:52:01 +0000

    added javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3205 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/creator/DtoCreator.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/creator/DtoCreator.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright 2004-2007 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,17 +21,48 @@ import org.seasar.framework.container.deployer.InstanceDefFactory;
 import org.seasar.framework.convention.NamingConvention;
 
 /**
- * Dto用のクリエータです。
+ * Dtoクラス用の {@link ComponentCreator}です。
+ * <p>
+ * 決められた命名規約に従って、クラスからDtoクラスのコンポーネント定義を作成します。 作成されるコンポーネント定義の各種属性は以下になります。
+ * 
+ * <table>
+ * <tr>
+ * <th>サフィックス</th>
+ * <td>{@link NamingConvention#getDtoSuffix() Dto(デフォルト)}</td>
+ * </tr>
+ * <tr>
+ * <th>インスタンス定義</th>
+ * <td>request</td>
+ * </tr>
+ * <tr>
+ * <th>自動バインディング</th>
+ * <td>無効</td>
+ * </tr>
+ * <tr>
+ * <th>外部バインディング</th>
+ * <td>無効</td>
+ * </tr>
+ * <tr>
+ * <th>インターフェース</th>
+ * <td>対象外</td>
+ * </tr>
+ * <tr>
+ * <th>抽象クラス</th>
+ * <td>対象外</td>
+ * </tr>
+ * </table>
+ * </p>
  * 
  * @author higa
+ * @author yatsu
  * 
  */
 public class DtoCreator extends ComponentCreatorImpl {
 
     /**
-     * {@link DtoCreator}を作成します。
+     * 指定された{@link NamingConvention 命名規約}に従った{@link DtoCreator}を作成します。
      * 
-     * @param namingConvention
+     * @param namingConvention 命名規約
      */
     public DtoCreator(NamingConvention namingConvention) {
         super(namingConvention);
@@ -41,18 +72,18 @@ public class DtoCreator extends ComponentCreatorImpl {
     }
 
     /**
-     * Dto用の {@link ComponentCustomizer}を返します。
+     * {@link ComponentCustomizer}を返します。
      * 
-     * @return
+     * @return コンポーネントカスタマイザ
      */
     public ComponentCustomizer getDtoCustomizer() {
         return getCustomizer();
     }
 
     /**
-     * Dto用の {@link ComponentCustomizer}を設定します。
+     * {@link ComponentCustomizer}を設定します。
      * 
-     * @param customizer
+     * @param customizer コンポーネントカスタマイザ
      */
     public void setDtoCustomizer(ComponentCustomizer customizer) {
         setCustomizer(customizer);
