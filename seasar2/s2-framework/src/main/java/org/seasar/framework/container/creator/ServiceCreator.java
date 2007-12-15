0b4b674e78baa9b754bb02fd6a99f1d0780beead
vestige <vestige@319488c0-e101-0410-93bc-b5e51f62721a>
2007-12-15 23:38:29 +0000

    javadoc add
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3328 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/creator/ServiceCreator.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/creator/ServiceCreator.java
@@ -22,16 +22,48 @@ import org.seasar.framework.convention.NamingConvention;
 
 /**
  * Service用の {@link ComponentCreator}です。
- * 
+  * <P>
+ * 決められた命名規約に従って、クラスからServiceクラスのコンポーネント定義を作成します。 作成されるコンポーネント定義の各種属性は以下になります。
+ *
+ * <table>
+ * <tr>
+ * <th>サフィックス</th>
+ * <td>{@link NamingConvention#getServiceSuffix() Service(デフォルト)}</td>
+ * </tr>
+ * <tr>
+ * <th>インスタンス定義</th>
+ * <td>prototype</td>
+ * </tr>
+ * <tr>
+ * <th>自動バインディング</th>
+ * <td>auto</td>
+ * </tr>
+ * <tr>
+ * <th>外部バインディング</th>
+ * <td>無効</td>
+ * </tr>
+ * <tr>
+ * <th>インターフェース</th>
+ * <td>有効</td>
+ * </tr>
+ * <tr>
+ * <th>抽象クラス</th>
+ * <td>有効</td>
+ * </tr>
+ * </table>
+ * </p>
+ *
  * @author higa
- * 
+ * @author vestige
+ *  
  */
 public class ServiceCreator extends ComponentCreatorImpl {
 
     /**
-     * {@link ServiceCreator}を作成します。
+     * 指定された{@link NamingConvention 命名規約}に従った{@link ServiceCreator}を作成します。
      * 
      * @param namingConvention
+     *          命名規約
      */
     public ServiceCreator(NamingConvention namingConvention) {
         super(namingConvention);
@@ -44,7 +76,7 @@ public class ServiceCreator extends ComponentCreatorImpl {
     /**
      * Service用の {@link ComponentCustomizer}を返します。
      * 
-     * @return
+     * @return コンポーネントカスタマイザ
      */
     public ComponentCustomizer getServiceCustomizer() {
         return getCustomizer();
@@ -53,7 +85,7 @@ public class ServiceCreator extends ComponentCreatorImpl {
     /**
      * Service用の {@link ComponentCustomizer}を設定します。
      * 
-     * @param customizer
+     * @param customizer コンポーネントカスタマイザ
      */
     public void setServiceCustomizer(ComponentCustomizer customizer) {
         setCustomizer(customizer);
