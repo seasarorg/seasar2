b67ff1aa3bf526b4c3d00e0647838a7dffc7e207
yatsu <yatsu@319488c0-e101-0410-93bc-b5e51f62721a>
2007-12-30 04:11:53 +0000

    added Javadoc comment.
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3361 319488c0-e101-0410-93bc-b5e51f62721a

--- seasar2/s2-framework/src/main/java/org/seasar/framework/container/creator/HelperCreator.java
+++ seasar2/s2-framework/src/main/java/org/seasar/framework/container/creator/HelperCreator.java
@@ -21,17 +21,49 @@ import org.seasar.framework.container.deployer.InstanceDefFactory;
 import org.seasar.framework.convention.NamingConvention;
 
 /**
- * Helper用の {@link ComponentCreator}です。
+ * Helperクラス用の {@link ComponentCreator}です。
+ * <p>
+ * 決められた命名規約に従って、クラスからHelperクラスのコンポーネント定義を作成します。 作成されるコンポーネント定義の各種属性は以下になります。
+ * 
+ * <table>
+ * <tr>
+ * <th>サフィックス</th>
+ * <td>{@link NamingConvention#getHelperSuffix() Helper(デフォルト)}</td>
+ * </tr>
+ * <tr>
+ * <th>インスタンス定義</th>
+ * <td>prototype</td>
+ * </tr>
+ * <tr>
+ * <th>自動バインディング</th>
+ * <td>auto</td>
+ * </tr>
+ * <tr>
+ * <th>外部バインディング</th>
+ * <td>無効</td>
+ * </tr>
+ * <tr>
+ * <th>インターフェース</th>
+ * <td>対象外</td>
+ * </tr>
+ * <tr>
+ * <th>抽象クラス</th>
+ * <td>対象外</td>
+ * </tr>
+ * </table>
+ * </p>
  * 
  * @author higa
+ * @author yatsu
  * 
  */
 public class HelperCreator extends ComponentCreatorImpl {
 
     /**
-     * {@link HelperCreator}を作成します。
+     * 指定された{@link NamingConvention 命名規約}に従った{@link HelperCreator}を作成します。
      * 
      * @param namingConvention
+     *            命名規約
      */
     public HelperCreator(NamingConvention namingConvention) {
         super(namingConvention);
@@ -40,18 +72,19 @@ public class HelperCreator extends ComponentCreatorImpl {
     }
 
     /**
-     * Helper用の {@link ComponentCustomizer}を返します。
+     * {@link ComponentCustomizer}を返します。
      * 
-     * @return
+     * @return コンポーネントカスタマイザ
      */
     public ComponentCustomizer getHelperCustomizer() {
         return getCustomizer();
     }
 
     /**
-     * Helper用の {@link ComponentCustomizer}を設定します。
+     * {@link ComponentCustomizer}を設定します。
      * 
      * @param customizer
+     *            コンポーネントカスタマイザ
      */
     public void setHelperCustomizer(ComponentCustomizer customizer) {
         setCustomizer(customizer);
