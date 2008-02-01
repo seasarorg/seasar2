ec0e26c50f01d95913947f89022af63280433587
Koichi Kobayashi <koichik@improvement.jp>
2008-02-01 19:49:07 +0000

    [CONTAINER-168] S2JDBC のチュートリアルに iterate() および getCount() を追加しました．[Seasar-user:12612] https://www.seasar.org/issues/browse/CONTAINER-168 http://ml.seasar.org/archives/seasar-user/2008-January/012612.html
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3481 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2jdbc-tutorial/src/test/java/examples/entity/IterateTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.entity;
+
+import org.seasar.extension.jdbc.IterationCallback;
+import org.seasar.extension.jdbc.IterationContext;
+import org.seasar.extension.jdbc.JdbcManager;
+import org.seasar.extension.unit.S2TestCase;
+
+/**
+ * @author koichik
+ */
+public class IterateTest extends S2TestCase {
+
+    private JdbcManager jdbcManager;
+
+    @Override
+    protected void setUp() throws Exception {
+        include("app.dicon");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testIterate() throws Exception {
+        long sum =
+            jdbcManager.from(Employee.class).iterate(
+                new IterationCallback<Employee, Long>() {
+                    private long sum;
+
+                    public Long iterate(Employee emp, IterationContext context) {
+                        sum += emp.salary;
+                        return sum;
+                    }
+                });
+        System.out.println(sum);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testIterateExit() throws Exception {
+        Employee emp =
+            jdbcManager.from(Employee.class).iterate(
+                new IterationCallback<Employee, Employee>() {
+                    private long sum;
+
+                    public Employee iterate(Employee emp,
+                            IterationContext context) {
+                        sum += emp.salary;
+                        if (sum > 10000) {
+                            context.setExit(true);
+                        }
+                        return emp;
+                    }
+                });
+        System.out.println(emp.name);
+    }
+
+}
