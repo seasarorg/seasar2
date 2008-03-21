47592e8d2717e8baf4e41ff711e28c3edb29c46d
Koichi Kobayashi <koichik@improvement.jp>
2008-03-21 12:08:40 +0000

    [CONTAINER-177] PostgreSQL8.1以降に対応した Dialect を追加する．[Seasar-user:13308] https://www.seasar.org/issues/browse/CONTAINER-177 http://ml.seasar.org/archives/seasar-user/2008-March/013308.html
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3533 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2-tiger/src/main/java/org/seasar/extension/jdbc/dialect/Postgre81Dialect.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.extension.jdbc.dialect;
+
+import org.seasar.extension.jdbc.SelectForUpdateType;
+import org.seasar.framework.util.tiger.Pair;
+
+/**
+ * PostgreSQL8.1用の方言をあつかうクラスです。
+ * 
+ * @author koichik
+ */
+public class Postgre81Dialect extends PostgreDialect {
+
+    @Override
+    public boolean supportsForUpdate(final SelectForUpdateType type,
+            boolean withTarget) {
+        return type != SelectForUpdateType.WAIT;
+    }
+
+    @Override
+    public String getForUpdateString(final SelectForUpdateType type,
+            final int waitSeconds, final Pair<String, String>... aliases) {
+        final String sql = super.getForUpdateString(type, waitSeconds, aliases);
+        return type == SelectForUpdateType.NOWAIT ? sql + " nowait" : sql;
+    }
+
+}
