d7b977f7aff5ed201dfb175e65ee94fc23351cd9
nakamura-to <toshihiro.nakamura@gmail.com>
2008-08-05 16:23:30 +0000

    AntタスクからCommandを呼び出す方法を見直し。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3714 319488c0-e101-0410-93bc-b5e51f62721a

--- s2jdbc-gen/s2jdbc-gen-core/src/main/java/org/seasar/extension/jdbc/gen/task/AbstractTask.java
+++ s2jdbc-gen/s2jdbc-gen-core/src/main/java/org/seasar/extension/jdbc/gen/task/AbstractTask.java
@@ -15,11 +15,16 @@
  */
 package org.seasar.extension.jdbc.gen.task;
 
+import java.lang.reflect.Method;
+
+import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.Path;
-import org.apache.tools.ant.types.Reference;
 import org.seasar.extension.jdbc.gen.Command;
+import org.seasar.extension.jdbc.gen.util.BeanUtil;
+import org.seasar.framework.util.ClassUtil;
+import org.seasar.framework.util.MethodUtil;
 
 /**
  * {@link Task}の抽象クラスです。
@@ -28,8 +33,11 @@ import org.seasar.extension.jdbc.gen.Command;
  */
 public abstract class AbstractTask extends Task {
 
-    /** クラスパス */
-    protected Path classpath;
+    /** クラスパスの参照ID */
+    protected static String CLASSPATH_REFID = "s2jdbc-gen-classpath";
+
+    /** クラスローダー */
+    protected static AntClassLoader classLoader;
 
     /**
      * インスタンスを構築します。
@@ -37,42 +45,65 @@ public abstract class AbstractTask extends Task {
     public AbstractTask() {
     }
 
-    /**
-     * クラスパスを設定します。
-     * 
-     * @param classpath
-     *            クラスパス
-     */
-    public void setClasspath(Path classpath) {
-        createClasspath().append(classpath);
-    }
-
-    /**
-     * クラスパスの参照を設定します。
-     * 
-     * @param reference
-     *            クラスパスの参照
-     */
-    public void setClasspathRef(Reference reference) {
-        createClasspath().setRefid(reference);
-    }
-
-    /**
-     * クラスパスを作成します。
-     * 
-     * @return クラスパス
-     */
-    public Path createClasspath() {
-        if (classpath == null) {
-            classpath = new Path(getProject());
-        }
-        return classpath.createPath();
-    }
-
     @Override
     public void execute() throws BuildException {
-        CommandExecutor executor = new CommandExecutor(classpath, getTaskName());
-        executor.execute(getCommand());
+        Object path = getProject().getReference(CLASSPATH_REFID);
+        if (path == null) {
+            throw new BuildException("reference '" + CLASSPATH_REFID
+                    + "' not found.");
+        }
+        if (!Path.class.isInstance(path)) {
+            throw new BuildException("reference type '" + CLASSPATH_REFID
+                    + "' is not instance of '" + Path.class.getName() + "'.");
+        }
+        setupClassLoader(Path.class.cast(path));
+
+        Class<?> targetClass = null;
+        classLoader.setThreadContextLoader();
+        try {
+            try {
+                targetClass = classLoader.forceLoadClass(getCommand()
+                        .getClass().getName());
+            } catch (ClassNotFoundException e) {
+                throw new BuildException(getCommand().getClass().getName()
+                        + " not found.", e);
+            }
+            Object targetCommand = ClassUtil.newInstance(targetClass);
+            copyProperties(getCommand(), targetCommand);
+            Method method = ClassUtil.getMethod(targetClass, "execute", null);
+            MethodUtil.invoke(method, targetCommand, null);
+        } finally {
+            classLoader.resetThreadContextLoader();
+        }
+    }
+
+    /**
+     * クラスローダをセットアップします。
+     * 
+     * @param path
+     *            パス
+     */
+    protected void setupClassLoader(Path path) {
+        if (classLoader != null) {
+            return;
+        }
+        classLoader = getProject().createClassLoader(
+                ClassLoader.getSystemClassLoader(), path);
+        classLoader.setParentFirst(false);
+        classLoader.addJavaLibraries();
+        classLoader.setIsolated(true);
+    }
+
+    /**
+     * JavaBeans形式のプロパティをコピーします。
+     * 
+     * @param src
+     *            コピー元のコマンド
+     * @param dest
+     *            コピー先のコマンド
+     */
+    protected void copyProperties(Command src, Object dest) {
+        BeanUtil.copy(src, dest);
     }
 
     /**
