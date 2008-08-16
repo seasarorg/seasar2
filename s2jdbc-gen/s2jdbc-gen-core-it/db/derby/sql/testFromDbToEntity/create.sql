359fb194e787642ac1db80d419f09f2afcd3860a
nakamura-to <toshihiro.nakamura@gmail.com>
2008-08-16 15:51:28 +0000

    Derbyのテストを追加。
    git-svn-id: https://www.seasar.org/svn/s2container/trunk@3755 319488c0-e101-0410-93bc-b5e51f62721a

---
+++ s2jdbc-gen/s2jdbc-gen-core-it/db/derby/sql/testFromDbToEntity/create.sql
@@ -0,0 +1,17 @@
+create table smallint_table (smallint_column smallint);
+create table integer_table (integer_column integer);
+create table bigint_table (bigint_column bigint);
+create table decimal_table (decimal_column decimal);
+create table real_table (real_column real);
+create table double_table (double_column double);
+create table char_table (char_column char);
+create table char_bit_table (char_bit_column char for bit data);
+create table varchar_table (varchar_column varchar(255));
+create table varchar_bit_table (varchar_bit_column varchar(255) for bit data);
+create table long_varchar_table (long_varchar_column long varchar);
+create table long_varchar_bit_table (long_varchar_bit_column long varchar for bit data);
+create table clob_table (clob_column clob);
+create table blob_table (blob_column blob);
+create table date_table (date_column date);
+create table time_table (time_column time);
+create table timestamp_table (timestamp_column timestamp);
